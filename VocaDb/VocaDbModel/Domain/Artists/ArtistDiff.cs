41cdde3f6f6252810a30c2654e94d9da5dce4f9c
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2011-11-14 21:46:44 +0000

    Began working on artist diffs

--- VocaDb/VocaDbModel/Domain/Artists/ArtistDiff.cs
+++ VocaDb/VocaDbModel/Domain/Artists/ArtistDiff.cs
@@ -1,12 +1,69 @@
-﻿namespace VocaDb.Model.Domain.Artists {
+﻿using System;
+using System.Linq;
+
+namespace VocaDb.Model.Domain.Artists {
 
 	public class ArtistDiff {
 
-		public ArtistDiff() {
-			IncludePicture = true;
+		private bool IsSet(ArtistEditableFields field) {
+			return ChangedFields.HasFlag(field);
+		}
+
+		private void Set(ArtistEditableFields field, bool val) {
+
+			if (val && !IsSet(field))
+				ChangedFields |= field;
+			else if (!val && IsSet(field))
+				ChangedFields -= field;
+
+		}
+
+		public ArtistDiff()
+			: this(true) { }
+
+		public ArtistDiff(bool isSnapshot) {
+			IsSnapshot = isSnapshot;
+		}
+
+		public virtual ArtistEditableFields ChangedFields { get; set; }
+
+		public virtual string ChangedFieldsString {
+			get {
+
+				var fieldNames = EnumVal<ArtistEditableFields>.Values.Where(f => f != ArtistEditableFields.Nothing && IsSet(f));
+				return string.Join(",", fieldNames);
+
+			}
+			set {
+
+				ChangedFields = ArtistEditableFields.Nothing;
+
+				if (string.IsNullOrEmpty(value)) {
+					return;
+				}
+
+				var fieldNames = value.Split(',');
+				foreach (var name in fieldNames) {
+					ArtistEditableFields field;
+					if (Enum.TryParse(name, out field))
+						Set(field, true);
+				}
+
+			}
 		}
 
 		public bool IncludePicture { get; set; }
+
+		public virtual bool IsSnapshot { get; set; }
+
+		public virtual bool Picture {
+			get {
+				return IsSet(ArtistEditableFields.Picture);
+			}
+			set {
+				Set(ArtistEditableFields.Picture, value);
+			}
+		}
 
 	}
 
---
+++ VocaDb/VocaDbModel/Domain/Artists/ArtistEditableFields.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VocaDb.Model.Domain.Artists {
+
+	[Flags]
+	public enum ArtistEditableFields {
+
+		Nothing		= 0,
+
+		ArtistType	= 1,
+
+		Description	= 2,
+
+		Groups		= 4,
+
+		Names		= 8,
+
+		Picture		= 16,
+
+		WebLinks	= 32
+
+	}
+
+}
