c557d7325d14eac91a49919b7aac97b1d16875bb
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2011-11-20 21:51:34 +0000

    Reworked notification panels

---
+++ VocaDb/VocaDbModel/Service/EntryValidators/AlbumValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Domain.Globalization;
+using VocaDb.Model.Resources;
+
+namespace VocaDb.Model.Service.EntryValidators {
+
+	public static class AlbumValidator {
+
+		public static ValidationResult Validate(Album album) {
+
+			ParamIs.NotNull(() => album);
+
+			var errors = new List<string>();
+
+			if (!album.Artists.Any())
+				errors.Add(AlbumValidationErrors.NeedArtist);
+
+			if (album.Names.Names.Any(n => n.Language == ContentLanguageSelection.Unspecified))
+				errors.Add(AlbumValidationErrors.UnspecifiedNames);
+
+			if (!album.Songs.Any())
+				errors.Add(AlbumValidationErrors.NeedTracks);
+
+			return new ValidationResult(errors);
+
+		}
+
+	}
+
+}
