dbc3343f65cd31ef09cbc9b051500e89a7752f0a
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2011-11-24 16:54:47 +0000

    Removed requirements for missing fields

--- VocaDb/VocaDbWeb/Models/ArtistModels.cs
+++ VocaDb/VocaDbWeb/Models/ArtistModels.cs
@@ -98,17 +98,14 @@ namespace VocaDb.Web.Models {
 		[Display(Name = "Names")]
 		public IList<LocalizedStringEdit> Names { get; set; }
 
-		[Required]
 		[Display(Name = "Name in English")]
 		[StringLength(255)]
 		public string NameEnglish { get; set; }
 
-		[Required]
 		[Display(Name = "Name in Japanese")]
 		[StringLength(255)]
 		public string NameJapanese { get; set; }
 
-		[Required]
 		[Display(Name = "Name in Romaji")]
 		[StringLength(255)]
 		public string NameRomaji { get; set; }
