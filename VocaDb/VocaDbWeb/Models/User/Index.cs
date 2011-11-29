72517a6f44c393552477e44b8ec5b5feaa94a2c8
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2011-11-29 19:57:22 +0000

    Display total number of entries in all main listings.

---
+++ VocaDb/VocaDbWeb/Models/User/Index.cs
@@ -0,0 +1,17 @@
+﻿using VocaDb.Model.DataContracts.Users;
+
+namespace VocaDb.Web.Models.User {
+
+	public class Index {
+
+		public Index() { }
+
+		public Index(UserContract[] users) {
+			Users = users;
+		}
+
+		public UserContract[] Users { get; set; }
+
+	}
+
+}
