8537da78c32c59e0f3d7163d52cd7a1e2573aff6
riipah <riipah@gmail.com>
2011-12-13 12:39:24 +0000

    Multiline search box for song merging (will replace other boxes too if this works)

--- VocaDb/VocaDbWeb/Scripts/Song/Merge.js
+++ VocaDb/VocaDbWeb/Scripts/Song/Merge.js
@@ -1,6 +1,20 @@
 ﻿
 function initPage(songId) {
 
+	var source = new Array();
+
+	$("#songList").jqxListBox({ width: '400', height: '350' });
+
+	$('#songList').bind('select', function (event) {
+
+		var item = $("#songList").jqxListBox('getItem', args.index);
+
+		if (item != null) {
+			$("#targetSongId").val(item.value);
+		}
+
+	}); 
+
 	$("input#songName").keyup(function () {
 
 		var findTerm = $(this).val();
@@ -15,23 +29,29 @@ function initPage(songId) {
 
 		$.post("../../Song/FindJsonByName", { term: findTerm }, function (results) {
 
-			$(songList).empty();
+			//$(songList).empty();
+			var rows = new Array();
 
 			$(results.Items).each(function () {
 
+
 				if (this.Id != songId) {
-					addOption(songList, this.Id, this.Name
-						+ (this.AdditionalNames != "" ? " (" + this.AdditionalNames + ")" : "") 
-						+ (this.ArtistString != "" ? " (by " + this.ArtistString + ")" : ""));
+					rows.push({ value: this.Id, html: "<div tabIndex=0 style='padding: 1px;'><input type='hidden' class='songId' value='" + this.Id + "' /><div>" + this.Name + "</div><div>" + this.ArtistString + "</div></div>", title: this.AdditionalNames });
+					//$(songList).jqxListBox('insertAt', "<div tabIndex=0 style='padding: 1px;'><div>" + this.Name + "</div><div>" + this.ArtistString + "</div></div>", 1);
+					/*addOption(songList, this.Id, this.Name
+					+ (this.AdditionalNames != "" ? " (" + this.AdditionalNames + ")" : "") 
+					+ (this.ArtistString != "" ? " (by " + this.ArtistString + ")" : ""));*/
 				}
 
 			});
+
+			$(songList).jqxListBox({ source: rows });
 
 		});
 
 	});
 
-	$("input:submit").click(function () {
+	$("#mergeBtn").click(function () {
 
 		return confirm("Are you sure you want to merge the songs?");
 
