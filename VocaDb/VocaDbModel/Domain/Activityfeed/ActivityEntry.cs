9570fa68775c3880acbccfc4b476c61f35985c6b
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-01-16 22:30:16 +0000

    Began working on activity feed

---
+++ VocaDb/VocaDbModel/Domain/Activityfeed/ActivityEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VocaDb.Model.Domain.Users;
+
+namespace VocaDb.Model.Domain.Activityfeed {
+
+	public class ActivityEntry {
+
+		private User author;
+
+		public virtual DateTime CreateDate { get; set; }
+
+		public virtual ActivityEntryType EntryType { get; set; }
+
+		public virtual bool Sticky { get; set; }
+
+	}
+}
