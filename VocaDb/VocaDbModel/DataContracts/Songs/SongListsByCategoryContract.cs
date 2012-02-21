e0c1a0730d5dd93e53ea35386ada9bf0a4cff268
riipah <riipah@gmail.com>
2012-02-21 12:07:39 +0000

    Finished featured song lists

---
+++ VocaDb/VocaDbModel/DataContracts/Songs/SongListsByCategoryContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VocaDb.Model.Domain.Songs;
+using VocaDb.Model.Domain.Globalization;
+using VocaDb.Model.Domain.Security;
+
+namespace VocaDb.Model.DataContracts.Songs {
+
+	public class SongListsByCategoryContract {
+
+		public SongListsByCategoryContract() { }
+
+		public SongListsByCategoryContract(SongListFeaturedCategory category, IEnumerable<SongList> lists, IUserPermissionContext permissionContext) {
+
+			Category = category;
+			Lists = lists.Select(l => new SongListContract(l, permissionContext)).ToArray();
+
+		}
+
+		public SongListFeaturedCategory Category { get; set; }
+
+		public SongListContract[] Lists { get; set; }
+
+	}
+}
