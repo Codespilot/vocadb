3902d9b636e3a18553f47437e3e0262089bd2107
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-03-26 20:54:45 +0000

    Working on events

---
+++ VocaDb/VocaDbModel/DataContracts/ReleaseEvents/ReleaseEventSeriesForEditContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VocaDb.Model.Domain.Albums;
+
+namespace VocaDb.Model.DataContracts.ReleaseEvents {
+
+	public class ReleaseEventSeriesForEditContract : ReleaseEventSeriesContract {
+
+		public ReleaseEventSeriesForEditContract() {}
+
+		public ReleaseEventSeriesForEditContract(ReleaseEventSeries series) : base(series) {
+
+			Aliases = series.Aliases.Select(a => a.Name).ToArray();
+
+		}
+
+		public string[] Aliases { get; set; }
+
+	}
+
+}
