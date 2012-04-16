c87f76cb45230b6d52e963cb793171b48cb48f2f
riipah <riipah@gmail.com>
2012-04-16 16:53:35 +0000

    Messages page translated.

--- VocaDb/VocaDbWeb/Models/User/ComposeMessage.cs
+++ VocaDb/VocaDbWeb/Models/User/ComposeMessage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using ViewRes.User;
 using VocaDb.Model.DataContracts.Users;
 
 namespace VocaDb.Web.Models.User {
@@ -11,19 +12,20 @@ namespace VocaDb.Web.Models.User {
 
 		public ComposeMessage() { }
 
-		[Display(Name = "Body")]
+		[Display(ResourceType = typeof(MessagesStrings), Name = "Body")]
 		[Required]
 		[StringLength(1000)]
 		public string Body { get; set; }
 
-		[Display(Name = "High priority")]
+		[Display(ResourceType = typeof(MessagesStrings), Name = "HighPriority")]
 		public bool HighPriority { get; set; }
 
-		[Display(Name = "To")]
+		[Display(ResourceType = typeof(MessagesStrings), Name = "To")]
 		[Required]
 		[StringLength(100)]
 		public string ReceiverName { get; set; }
 
+		[Display(ResourceType = typeof(MessagesStrings), Name = "Subject")]
 		[Required]
 		[StringLength(200)]
 		public string Subject { get; set; }
