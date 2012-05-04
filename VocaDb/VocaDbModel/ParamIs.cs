e02b867fb5beda4cb2febc586d10bea41bca823a
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-05-04 21:26:06 +0000

    Optimized implementation of ParamIs

--- VocaDb/VocaDbModel/ParamIs.cs
+++ VocaDb/VocaDbModel/ParamIs.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace VocaDb.Model {
 
@@ -73,6 +77,7 @@ namespace VocaDb.Model {
 
 		}
 
+		/*
 		/// <summary>
 		/// Validates that the given parameter is not null.
 		/// </summary>
@@ -88,6 +93,27 @@ namespace VocaDb.Model {
 				var body = (MemberExpression)expression.Body;
 				throw new ArgumentNullException(body.Member.Name);				
 			}
+
+		}*/
+
+		/// <summary>
+		/// Validates that the given parameter is not null.
+		/// </summary>
+		/// <param name="arg">Parameter expression. Expression body must be a member expression, for example "() => param". Cannot be null.</param>
+		public static void NotNull<T>(Func<T> arg) where T : class {
+
+			if (arg() != null)
+				return;
+
+			var test = new FieldInfoReader<T>(arg);
+
+			FieldInfo fieldInfo = test.GetFieldToken();
+
+			if (fieldInfo == null) {
+				throw new ValidationException("No field info found in delegate");
+			}
+
+			throw new ArgumentNullException(fieldInfo.Name);
 
 		}
 
@@ -106,6 +132,7 @@ namespace VocaDb.Model {
 
 		}
 
+		/*
 		/// <summary>
 		/// Validates that the given parameter is not null or empty.
 		/// </summary>
@@ -121,6 +148,27 @@ namespace VocaDb.Model {
 				var body = (MemberExpression)expression.Body;
 				throw new ArgumentException(body.Member.Name + " cannot be null or empty", body.Member.Name);
 			}
+
+		}*/
+
+		/// <summary>
+		/// Validates that the given parameter is not null or empty.
+		/// </summary>
+		/// <param name="arg">Parameter expression. Expression body must be a member expression. Cannot be null.</param>
+		public static void NotNullOrEmpty(Func<string> arg) {
+
+			if (!string.IsNullOrEmpty(arg()))
+				return;
+
+			var test = new FieldInfoReader<string>(arg);
+
+			FieldInfo fieldInfo = test.GetFieldToken();
+
+			if (fieldInfo == null) {
+				throw new ValidationException("No field info found in delegate");
+			}
+
+			throw new ArgumentException(fieldInfo.Name + " cannot be null or empty");
 
 		}
 
@@ -154,5 +202,160 @@ namespace VocaDb.Model {
 
 		}
 
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="TParameter"></typeparam>
+	/// <remarks>Code from http://bronumski.blogspot.com/2010/06/taking-pain-out-of-parameter-validation.html </remarks>
+	internal class FieldInfoReader<TParameter> {
+		private readonly Func<TParameter> arg;
+
+		internal FieldInfoReader(Func<TParameter> arg) {
+			this.arg = arg;
+		}
+
+		public FieldInfo GetFieldToken() {
+			byte[] methodBodyIlByteArray = GetMethodBodyIlByteArray();
+
+			int fieldToken = GetFieldToken(methodBodyIlByteArray);
+
+			return GetFieldInfo(fieldToken);
+		}
+
+		private FieldInfo GetFieldInfo(int fieldToken) {
+			FieldInfo fieldInfo = null;
+
+			if (fieldToken > 0) {
+				Type argType = arg.Target.GetType();
+				Type[] genericTypeArguments = GetSubclassGenericTypes(argType);
+				Type[] genericMethodArguments = arg.Method.GetGenericArguments();
+
+				fieldInfo = argType.Module.ResolveField(fieldToken, genericTypeArguments, genericMethodArguments);
+			}
+
+			return fieldInfo;
+		}
+
+		private static OpCode GetOpCode(byte[] methodBodyIlByteArray, ref int currentPosition) {
+			ushort value = methodBodyIlByteArray[currentPosition++];
+
+			return value != 0xfe ? SingleByteOpCodes[value] : OpCodes.Nop;
+		}
+
+		private static int GetFieldToken(byte[] methodBodyIlByteArray) {
+			int position = 0;
+
+			while (position < methodBodyIlByteArray.Length) {
+				OpCode code = GetOpCode(methodBodyIlByteArray, ref position);
+
+				if (code.OperandType == OperandType.InlineField) {
+					return ReadInt32(methodBodyIlByteArray, ref position);
+				}
+
+				position = MoveToNextPosition(position, code);
+			}
+
+			return 0;
+		}
+
+		private static int MoveToNextPosition(int position, OpCode code) {
+			switch (code.OperandType) {
+				case OperandType.InlineNone:
+					break;
+
+				case OperandType.InlineI8:
+				case OperandType.InlineR:
+					position += 8;
+					break;
+
+				case OperandType.InlineField:
+				case OperandType.InlineBrTarget:
+				case OperandType.InlineMethod:
+				case OperandType.InlineSig:
+				case OperandType.InlineTok:
+				case OperandType.InlineType:
+				case OperandType.InlineI:
+				case OperandType.InlineString:
+				case OperandType.InlineSwitch:
+				case OperandType.ShortInlineR:
+					position += 4;
+					break;
+
+				case OperandType.InlineVar:
+					position += 2;
+					break;
+
+				case OperandType.ShortInlineBrTarget:
+				case OperandType.ShortInlineI:
+				case OperandType.ShortInlineVar:
+					position++;
+					break;
+
+				default:
+					throw new InvalidOperationException("Unknown operand type.");
+			}
+			return position;
+		}
+
+		private byte[] GetMethodBodyIlByteArray() {
+			MethodBody methodBody = arg.Method.GetMethodBody();
+
+			if (methodBody == null) {
+				throw new InvalidOperationException();
+			}
+
+			return methodBody.GetILAsByteArray();
+		}
+
+		private static int ReadInt32(byte[] il, ref int position) {
+			return ((il[position++] | (il[position++] << 8)) | (il[position++] << 0x10)) | (il[position++] << 0x18);
+		}
+
+		private static Type[] GetSubclassGenericTypes(Type toCheck) {
+			var genericArgumentsTypes = new List<Type>();
+
+			while (toCheck != null) {
+				if (toCheck.IsGenericType) {
+					genericArgumentsTypes.AddRange(toCheck.GetGenericArguments());
+				}
+
+				toCheck = toCheck.BaseType;
+			}
+
+			return genericArgumentsTypes.ToArray();
+		}
+
+		private static OpCode[] singleByteOpCodes;
+
+		public static OpCode[] SingleByteOpCodes {
+			get {
+				if (singleByteOpCodes == null) {
+					LoadOpCodes();
+				}
+				return singleByteOpCodes;
+			}
+		}
+
+		private static void LoadOpCodes() {
+			singleByteOpCodes = new OpCode[0x100];
+
+			FieldInfo[] opcodeFieldInfos = typeof(OpCodes).GetFields();
+
+			for (int i = 0; i < opcodeFieldInfos.Length; i++) {
+				FieldInfo info1 = opcodeFieldInfos[i];
+
+				if (info1.FieldType == typeof(OpCode)) {
+					var singleByteOpCode = (OpCode)info1.GetValue(null);
+
+					var singleByteOpcodeIndex = (ushort)singleByteOpCode.Value;
+
+					if (singleByteOpcodeIndex < 0x100) {
+						singleByteOpCodes[singleByteOpcodeIndex] = singleByteOpCode;
+					}
+				}
+			}
+		}
 	}
 }
