bdd11854b9d130bf5da2abbbafaf4937d4250576
riipah <riipah@gmail.com>
2012-05-24 22:35:14 +0000

    Filtering and sorting to user lists.

--- VocaDb/VocaDbWeb/Models/User/Index.cs
+++ VocaDb/VocaDbWeb/Models/User/Index.cs
@@ -1,4 +1,5 @@
 ﻿using VocaDb.Model.DataContracts.Users;
+using VocaDb.Model.Domain.Users;
 
 namespace VocaDb.Web.Models.User {
 
@@ -6,9 +7,12 @@ namespace VocaDb.Web.Models.User {
 
 		public Index() { }
 
-		public Index(UserContract[] users) {
+		public Index(UserContract[] users, UserGroupId groupId) {
 			Users = users;
+			GroupId = groupId;
 		}
+
+		public UserGroupId GroupId { get; set; }
 
 		public UserContract[] Users { get; set; }
 
