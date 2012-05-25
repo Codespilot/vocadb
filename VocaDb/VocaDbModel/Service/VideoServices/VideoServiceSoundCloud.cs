956a642e59769468e2624c4e007b13da8b210b3a
riipah <riipah@gmail.com>
2012-05-25 21:12:24 +0000

    Added support for SoundCloud

---
+++ VocaDb/VocaDbModel/Service/VideoServices/VideoServiceSoundCloud.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using VocaDb.Model.Domain.PVs;
+
+namespace VocaDb.Model.Service.VideoServices {
+
+	public class VideoServiceSoundCloud : VideoService {
+
+		public VideoServiceSoundCloud(PVService service, IVideoServiceParser parser, RegexLinkMatcher[] linkMatchers) 
+			: base(service, parser, linkMatchers) {}
+
+		public override string GetUrlById(string id) {
+
+			var compositeId = new SoundCloudId(id);
+			var matcher = linkMatchers.First();
+			return "http://" + matcher.MakeLinkFromId(compositeId.SoundCloudUrl);
+
+		}
+
+		public VideoUrlParseResult ParseBySoundCloudUrl(string url) {
+
+			var apiUrl = string.Format("http://api.soundcloud.com/resolve?url=http://soundcloud.com/{0}&client_id=YOUR_CLIENT_ID", url);
+
+			var request = WebRequest.Create(apiUrl);
+			XDocument doc;
+
+			using (var response = request.GetResponse())
+			using (var stream = response.GetResponseStream()) {
+				doc = XDocument.Load(stream);
+			}
+
+			var trackId = doc.XPathSelectElement("//track/id").Value;
+			var title = doc.XPathSelectElement("//track/title").Value;
+			var id = new SoundCloudId(trackId, url);
+
+			return new VideoUrlParseResult(PVService.SoundCloud, id.ToString(), title);
+
+		}
+
+		public override VideoUrlParseResult ParseByUrl(string url) {
+
+			var soundCloudUrl = linkMatchers[0].GetId(url);
+
+			return ParseBySoundCloudUrl(soundCloudUrl);
+
+		}
+
+	}
+
+	public class SoundCloudId {
+
+		public SoundCloudId(string trackId, string soundCloudUrl) {
+
+			ParamIs.NotNullOrEmpty(() => trackId);
+			ParamIs.NotNullOrEmpty(() => soundCloudUrl);
+
+			TrackId = trackId;
+			SoundCloudUrl = soundCloudUrl;
+
+		}
+
+		public SoundCloudId(string compositeId) {
+
+			ParamIs.NotNull(() => compositeId);
+
+			var parts = compositeId.Split(' ');
+
+			if (parts.Length < 2) {
+				throw new ArgumentException("Composite ID must contain both track Id and URL");
+			}
+
+			TrackId = parts[0];
+			SoundCloudUrl = parts[1];
+
+		}
+
+		public string SoundCloudUrl { get; set; }
+
+		public string TrackId { get; set; }
+
+		public override string  ToString() {
+			return string.Format("{0} {1}", TrackId, SoundCloudUrl);
+		}
+
+	}
+
+}
--- VocaDb/VocaDbModel/Service/VideoServices/YoutubeParser.cs
+++ VocaDb/VocaDbModel/Service/VideoServices/YoutubeParser.cs
@@ -1,6 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Text;
+using Google.YouTube;
 using HtmlAgilityPack;
 
 namespace VocaDb.Model.Service.VideoServices {
@@ -9,68 +11,18 @@ namespace VocaDb.Model.Service.VideoServices {
 
 		public VideoTitleParseResult GetTitle(string id) {
 
-			var url = string.Format("http://youtu.be/{0}", id);
-
-			string videoTitle = null;
-			var request = WebRequest.Create(url);
-			WebResponse response;
+			var settings = new YouTubeRequestSettings("MikuBot", null);
+			var request = new YouTubeRequest(settings);
+			var videoEntryUrl = new Uri(string.Format("http://gdata.youtube.com/feeds/api/videos/{0}", id));
 
 			try {
-				response = request.GetResponse();
-			} catch (WebException x) {
-				return VideoTitleParseResult.CreateError("Youtube (error): " + x.Message);
-			}
-
-			try {
-				var enc = response.Headers[HttpResponseHeader.ContentEncoding];
-
-				using (var stream = response.GetResponseStream()) {
-					videoTitle = GetVideoData(stream, enc);
-				}
-			} finally {
-				response.Close();
-			}
-
-			if (!string.IsNullOrEmpty(videoTitle)) {
-				return VideoTitleParseResult.CreateSuccess(videoTitle);
-			} else {
-				return VideoTitleParseResult.CreateError("Title element not found");
+				var video = request.Retrieve<Video>(videoEntryUrl);
+				return VideoTitleParseResult.CreateSuccess(video.Title);
+			} catch (Exception x) {
+				return VideoTitleParseResult.CreateError(x.Message);
 			}
 
 		}
-
-		private string GetVideoTitle(HtmlDocument doc) {
-
-			var titleElem = doc.DocumentNode.SelectSingleNode("//span[@id = 'eow-title']");
-			string titleText = null;
-
-			if (titleElem != null)
-				titleText = titleElem.GetAttributeValue("title", null);
-			else {
-
-				var verifyElem = doc.DocumentNode.SelectSingleNode("//meta[@name = 'title']");
-
-				if (verifyElem != null)
-					titleText = verifyElem.GetAttributeValue("content", null);
-
-			}
-
-			return HtmlEntity.DeEntitize(titleText);
-
-		}
-
-		private string GetVideoData(Stream htmlStream, string encodingStr) {
-
-			var encoding = (!string.IsNullOrEmpty(encodingStr) ? Encoding.GetEncoding(encodingStr) : Encoding.UTF8);
-
-			var doc = new HtmlDocument();
-			doc.Load(htmlStream, encoding);
-
-			// Video title element (could use page title as well...)
-			return GetVideoTitle(doc);
-
-		}
-
 
 	}
 
