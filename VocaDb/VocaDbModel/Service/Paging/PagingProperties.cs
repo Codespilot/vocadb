f7bc956ad7ee6ff5fda158362c7cc5c40f384658
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-06-03 13:36:08 +0000

    Some UI improvements, translations.

---
+++ VocaDb/VocaDbModel/Service/Paging/PagingProperties.cs
@@ -0,0 +1,24 @@
+﻿namespace VocaDb.Model.Service.Paging {
+
+	public class PagingProperties {
+
+		public static PagingProperties CreateFromPage(int page, int entriesPerPage, bool getTotalCount) {
+
+			return new PagingProperties(page * entriesPerPage, entriesPerPage, getTotalCount);
+
+		}
+
+		public PagingProperties(int start, int maxEntries, bool getTotalCount) {
+			Start = start;
+			MaxEntries = maxEntries;
+			GetTotalCount = getTotalCount;
+		}
+
+		public bool GetTotalCount { get; private set; }
+
+		public int MaxEntries { get; private set; }
+
+		public int Start { get; private set; }
+
+	}
+}
