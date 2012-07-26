e7513d97ac030be2c69c9acde47f519be1e556f5
riipah <riipah@gmail.com>
2012-07-26 21:15:20 +0000

    Ignore entry routes with strings (because for some reason bots seem to like those)

--- VocaDb/VocaDbWeb/Global.asax.cs
+++ VocaDb/VocaDbWeb/Global.asax.cs
@@ -117,13 +117,25 @@ namespace VocaDb.Web {
 
 		public static void RegisterRoutes(RouteCollection routes) {
 
+			// Ignored files
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 			routes.IgnoreRoute("favicon.ico");
 
-			routes.MapRoute("Search", "Search/{filter}", new { controller = "Home", action = "Search", filter = UrlParameter.Optional });
-			routes.MapRoute("Song", "S/{id}", new { controller = "Song", action = "Details" });
+			// Invalid routes
+			routes.MapRoute("AlbumDetailsError", "Album/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = "[a-z]+" });
+			routes.MapRoute("ArtistDetailsError", "Artist/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = "[a-z]+" });
+			routes.MapRoute("SongDetailsError", "Song/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = "[a-z]+" });
+
+			// Special routes
+			routes.MapRoute("Search", "Search/{filter}", 
+				new { controller = "Home", action = "Search", filter = UrlParameter.Optional });
+			routes.MapRoute("Song", "S/{id}", new { controller = "Song", action = "Details" }, new { id = "[0-9]+" });
 			routes.MapRoute("User", "Profile/{id}", new { controller = "User", action = "Profile" });
 
+			// Default mapping
 			routes.MapRoute(
 				"Default", // Route name
 				"{controller}/{action}/{id}", // URL with parameters
