5a3a40254969e6f39e984019b332d2bda88089b8
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-08-09 21:13:59 +0000

    Universal search returns tags as well

--- VocaDb/VocaDbModel/Service/Helpers/QueryableExtender.cs
+++ VocaDb/VocaDbModel/Service/Helpers/QueryableExtender.cs
@@ -9,11 +9,12 @@ namespace VocaDb.Model.Service.Helpers {
 
 	public static class QueryableExtender {
 
-		public static IQueryable<ArtistName> AddArtistNameFilter(this IQueryable<ArtistName> query, string originalQuery, string canonizedName, NameMatchMode matchMode) {
+		public static IQueryable<ArtistName> AddArtistNameFilter(this IQueryable<ArtistName> query, string originalQuery, 
+			string canonizedName, NameMatchMode matchMode, string[] words = null) {
 
 			canonizedName = canonizedName ?? ArtistHelper.GetCanonizedName(originalQuery);
 
-			if (matchMode == NameMatchMode.Exact || (matchMode == NameMatchMode.Auto && originalQuery.Length < 3)) {
+			if (FindHelpers.ExactMatch(originalQuery, matchMode)) {
 
 				return query.Where(m => m.Value == canonizedName 
 					|| m.Value == string.Format("{0}P", canonizedName) 
@@ -21,7 +22,7 @@ namespace VocaDb.Model.Service.Helpers {
 
 			} else {
 
-				return FindHelpers.AddEntryNameFilter(query, originalQuery, matchMode);
+				return FindHelpers.AddEntryNameFilter(query, canonizedName, matchMode, words);
 
 			}
 
