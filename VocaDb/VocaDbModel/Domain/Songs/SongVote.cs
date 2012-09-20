89d582459beefd1b2f192a573197c04ceccbc061
riipah <riipah@gmail.com>
2012-09-20 17:35:22 +0000

    Added new resource files

---
+++ VocaDb/VocaDbModel/Domain/Songs/SongVote.cs
@@ -0,0 +1,29 @@
+﻿using VocaDb.Model.Domain.Users;
+
+namespace VocaDb.Model.Domain.Songs {
+
+	public class SongVote {
+
+		private string hostname;
+		private Song song;
+
+		public virtual string Hostname {
+			get { return hostname; }
+			set { 
+				ParamIs.NotNullOrEmpty(() => value);
+				hostname = value; 
+			}
+		}
+
+		public virtual Song Song {
+			get { return song; }
+			set { 
+				ParamIs.NotNull(() => value);
+				song = value; 
+			}
+		}
+
+		public virtual User User { get; set; }
+
+	}
+}
