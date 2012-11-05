90570e37becd6d44f57a7ecd22aa268a8d01c85c
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-11-05 19:26:37 +0000

    Display song titles for PV viewer

--- VocaDb/VocaDbWeb/Scripts/Home/Index.js
+++ VocaDb/VocaDbWeb/Scripts/Home/Index.js
@@ -7,11 +7,14 @@ $(document).ready(function () {
         $("#songs-navi .item").removeClass("active");
         $(this).addClass("active");
 
+        var item = this;
         var id = getId(this);
         var songId = $(this).find(".songId").val();
         $.post("../Song/PVForSong", { pvId: id }, function (content) {
             $("#songPVPlayer").html(content);
             $("#songLink").attr("href", "../Song/Details/" + songId);
+            $("#songPreviewName").text($(item).find(".songName").text());
+            $("#songPreviewArtists").text($(item).find(".songArtists").text());
         });
 
     });
