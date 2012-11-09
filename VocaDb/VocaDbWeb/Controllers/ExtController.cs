e0dd3470d8eb829ce656e1792e1d4deee06fa7b4
riipah <riipah@gmail.com>
2012-11-09 17:43:02 +0000

    84: Song embeds

---
+++ VocaDb/VocaDbWeb/Controllers/ExtController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace VocaDb.Web.Controllers
+{
+    public class ExtController : ControllerBase
+    {
+        //
+        // GET: /Ext/
+
+        public ActionResult EmbedSong(int songId) {
+
+			var song = Services.Songs.GetSongWithPVAndVote(songId);
+
+			return PartialView(song);
+
+		}
+
+    }
+}
