bc983a52d73b68554c2fdac3f03dd805ef6e97c1
riipah <riipah@gmail.com>
2012-11-29 20:36:09 +0000

    Assume http:// prefix for external links

--- VocaDb/VocaDbModel/Service/Helpers/UrlHelper.cs
+++ VocaDb/VocaDbModel/Service/Helpers/UrlHelper.cs
@@ -4,7 +4,16 @@ namespace VocaDb.Model.Service.Helpers {
 
 	public static class UrlHelper {
 
+		/// <summary>
+		/// Makes a proper URL from a possible URL without a http:// prefix.
+		/// </summary>
+		/// <param name="partialLink">Partial URL. Can be null.</param>
+		/// <param name="assumeWww">Whether to assume the URL should start with www.</param>
+		/// <returns>Full URL including http://. Can be null if source was null.</returns>
 		public static string MakeLink(string partialLink, bool assumeWww = false) {
+
+			if (string.IsNullOrEmpty(partialLink))
+				return partialLink;
 
 			if (partialLink.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase) || partialLink.StartsWith("https://", StringComparison.InvariantCultureIgnoreCase))
 				return partialLink;
