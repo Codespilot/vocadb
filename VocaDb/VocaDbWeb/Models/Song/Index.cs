e56ed1160fd80a9c3025f44c35171f02324b9441
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2012-12-02 14:27:35 +0000

    reworked song param handling for the index page

--- VocaDb/VocaDbWeb/Models/Song/Index.cs
+++ VocaDb/VocaDbWeb/Models/Song/Index.cs
@@ -16,7 +16,7 @@ namespace VocaDb.Web.Models.Song {
 
 		public Index(PartialFindResult<SongWithAlbumAndPVsContract> result, string filter, NameMatchMode nameMatchMode, SongType songType, string timeFilter, bool onlyWithPVs, 
 			SongSortRule sortRule, SongViewMode viewMode, 
-			bool draftsOnly, int page, int pageSize) {
+			bool draftsOnly, int page, int pageSize, IndexRouteParams routeParams) {
 
 			DraftsOnly = draftsOnly;
 			Songs = new StaticPagedList<SongWithAlbumAndPVsContract>(result.Items, 
@@ -28,6 +28,7 @@ namespace VocaDb.Web.Models.Song {
 			OnlyWithPVs = onlyWithPVs;
 			SortRule = sortRule;
 			ViewMode = viewMode;
+			RouteParams = routeParams;
 
 		}
 
@@ -42,6 +43,8 @@ namespace VocaDb.Web.Models.Song {
 
 		public bool OnlyWithPVs { get; set; }
 
+		public IndexRouteParams RouteParams { get; set; }
+
 		public string Since { get; set; }
 
 		public IPagedList<SongWithAlbumAndPVsContract> Songs { get; set; }
@@ -52,57 +55,56 @@ namespace VocaDb.Web.Models.Song {
 
 		public SongViewMode ViewMode { get; set; }
 
-	}
-
-	public class IndexParams {
-
-		public IndexParams() {
-
-			matchMode = NameMatchMode.Auto;
-			page = 1;
-			pageSize = 30;
-			sort = SongSortRule.Name;
-			songType = SongType.Unspecified;
-
+		public IndexRouteParams CreateRouteParams(int page) {
+			return new IndexRouteParams(RouteParams, page);
 		}
 
-		public IndexParams(Index index, int page)
+	}
+
+	/// <summary>
+	/// Parameter collection given to index action.
+	/// </summary>
+	public class IndexRouteParams {
+
+		public IndexRouteParams() {}
+
+		public IndexRouteParams(IndexRouteParams index, int? page)
 			: this() {
 
 			ParamIs.NotNull(() => index);
 
-			draftsOnly = index.DraftsOnly;
-			filter = index.Filter;
-			matchMode = index.NameMatchMode;
-			onlyWithPVs = index.OnlyWithPVs;
-			pageSize = Math.Min(index.Songs.PageSize, 30);
-			since = index.Since;
-			songType = index.SongType;
-			sort = index.SortRule;
-			view = index.ViewMode;
+			draftsOnly = index.draftsOnly;
+			filter = index.filter;
+			matchMode = index.matchMode;
+			onlyWithPVs = index.onlyWithPVs;
+			pageSize = index.pageSize;
+			since = index.since;
+			songType = index.songType;
+			sort = index.sort;
+			view = index.view;
 			this.page = page;
 
 		}
 
-		public bool draftsOnly { get; set; }
+		public bool? draftsOnly { get; set; }
 
 		public string filter { get; set; }
 
-		public NameMatchMode matchMode { get; set; }
+		public NameMatchMode? matchMode { get; set; }
 
-		public bool onlyWithPVs { get; set; }
+		public bool? onlyWithPVs { get; set; }
 
-		public int page { get; set; }
+		public int? page { get; set; }
 
-		public int pageSize { get; set; }
+		public int? pageSize { get; set; }
 
 		public string since { get; set; }
 
-		public SongType songType { get; set; }
+		public SongType? songType { get; set; }
 
-		public SongSortRule sort { get; set; }
+		public SongSortRule? sort { get; set; }
 
-		public SongViewMode view { get; set; }
+		public SongViewMode? view { get; set; }
 
 	}
 
