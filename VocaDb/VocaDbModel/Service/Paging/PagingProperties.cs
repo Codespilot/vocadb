98812752fac946def274875a01e8b0143c45b60f
riipah <riipah@gmail.com>
2012-12-03 19:19:51 +0000

    Added Japanese UTAU wiki as external site. Added tooltips for artist groups.

--- VocaDb/VocaDbModel/Service/Paging/PagingProperties.cs
+++ VocaDb/VocaDbModel/Service/Paging/PagingProperties.cs
@@ -6,6 +6,13 @@ namespace VocaDb.Model.Service.Paging {
 	[DataContract(Namespace = Schemas.VocaDb)]
 	public class PagingProperties {
 
+		/// <summary>
+		/// Creates paging properties based on a page number (instead of absolute entry index).
+		/// </summary>
+		/// <param name="page">Page number (starting from 0)</param>
+		/// <param name="entriesPerPage">Number of entries per page.</param>
+		/// <param name="getTotalCount">Whether to get total count.</param>
+		/// <returns>Paging properties. Cannot be null.</returns>
 		public static PagingProperties CreateFromPage(int page, int entriesPerPage, bool getTotalCount) {
 
 			return new PagingProperties(page * entriesPerPage, entriesPerPage, getTotalCount);
