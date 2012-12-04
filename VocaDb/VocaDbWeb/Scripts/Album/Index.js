a2fbdc19425d82a8546700fced7a6b62554d6c1c
riipah <riipah@gmail.com>
2012-12-04 19:29:37 +0000

    Some error handling. Tooltips for albums in tiled mode. Display specific entry type name (for example Vocaloid) in title instead of generic type (Artist).

---
+++ VocaDb/VocaDbWeb/Scripts/Album/Index.js
@@ -0,0 +1,6 @@
+﻿
+$(document).ready(function () {
+	$("#createLink").button({ disabled: $("#createLink").hasClass("disabled"), icons: { primary: 'ui-icon-plusthick' } });
+	$("#manageDeletedLink").button({ icons: { primary: 'ui-icon-trash' } });
+	$("#albums img").vdbAlbumToolTip();
+});
