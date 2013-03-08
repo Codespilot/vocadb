c2d394d9b23f7dcf3bb4ec854360428a0f36a9df
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-03-08 22:31:26 +0000

    Working on external tooltips

--- VocaDb/VocaDbWeb/Controllers/ExtController.cs
+++ VocaDb/VocaDbWeb/Controllers/ExtController.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
 using VocaDb.Model.Domain.Globalization;
+using VocaDb.Model.Utils;
+using VocaDb.Web.Helpers;
 
 namespace VocaDb.Web.Controllers
 {
@@ -21,6 +25,22 @@ namespace VocaDb.Web.Controllers
 
 			return PartialView(song);
 
+		}
+
+		public ActionResult EntryToolTip(string url) {
+
+			var route = new RouteInfo(new Uri(url), AppConfig.HostAddress).RouteData;
+			var controller = route.Values["controller"].ToString();
+			var id = int.Parse(route.Values["id"].ToString());
+
+			switch (controller) {
+				case "Album":
+					return RedirectToAction("PopupContent", "Album", new { id });
+				case "Artist":
+					return RedirectToAction("PopupContent", "Artist", new {id});
+			}
+
+			return Content("");
 		}
 
     }
