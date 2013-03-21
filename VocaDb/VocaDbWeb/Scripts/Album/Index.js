a2fe41c9b780121926b91018fc414aba22c6e88f
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-03-21 20:27:02 +0000

    Added hint tooltips to artist and album views too

--- VocaDb/VocaDbWeb/Scripts/Album/Index.js
+++ VocaDb/VocaDbWeb/Scripts/Album/Index.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
 	$("#createLink").button({ disabled: $("#createLink").hasClass("disabled"), icons: { primary: 'ui-icon-plusthick' } });
 	$("#manageDeletedLink").button({ icons: { primary: 'ui-icon-trash' } });
 	$("#albums img").vdbAlbumToolTip();
+	$("#filterBox").tooltip();
 });
