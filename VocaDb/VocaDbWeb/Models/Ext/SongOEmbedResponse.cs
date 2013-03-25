160058574cae1a5f7fff3ffd6034651d8b2cb268
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-03-25 22:38:24 +0000

    Tweaked OEmbed

--- VocaDb/VocaDbWeb/Models/Ext/SongOEmbedResponse.cs
+++ VocaDb/VocaDbWeb/Models/Ext/SongOEmbedResponse.cs
@@ -6,28 +6,34 @@ namespace VocaDb.Web.Models.Ext {
 
 		public int height {
 			get { return 400; }
+			set { }
 		}
 
 		public string html { get; set; }
 
 		public string provider_name {
 			get { return "VocaDB"; }
+			set { }
 		}
 
 		public string provider_url {
 			get { return AppConfig.HostAddress; }
+			set { }
 		}
 
 		public string type {
 			get { return "video"; }
+			set { }
 		}
 
 		public string version {
 			get { return "1.0"; }
+			set { }
 		}
 
 		public int width {
 			get { return 570; }
+			set { }
 		}
 
 	}
