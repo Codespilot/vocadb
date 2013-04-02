261b6f398dafaa22f059c7932fb85cca6fbc21a9
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-04-02 16:19:47 +0000

    Reverted back to VocaDB

---
+++ VocaDb/Tests/Service/VideoServices/NicoHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaDb.Model.Domain.Songs;
+using VocaDb.Model.Service.VideoServices;
+
+namespace VocaDb.Tests.Service.VideoServices {
+
+	/// <summary>
+	/// Unit tests for <see cref="NicoHelper"/>.
+	/// </summary>
+	[TestClass]
+	public class NicoHelperTests {
+
+		[TestInitialize]
+		public void SetUp() {
+			
+		}
+
+		[TestMethod]
+		public void ParseTitle_Valid() {
+
+			var result = NicoHelper.ParseTitle("【重音テト】 ハイゲインワンダーランド 【オリジナル】");
+
+			Assert.AreEqual(1, result.ArtistNames.Length, "1 artist");
+			Assert.AreEqual("重音テト", result.ArtistNames.First(), "artist");
+			Assert.AreEqual("ハイゲインワンダーランド", result.Title, "title");
+			Assert.AreEqual(SongType.Original, result.SongType, "song type");
+
+		}
+
+	}
+
+}
---
+++ VocaDb/VocaDbModel/Service/VideoServices/NicoTitleParseResult.cs
@@ -0,0 +1,23 @@
+﻿using VocaDb.Model.Domain.Songs;
+
+namespace VocaDb.Model.Service.VideoServices {
+
+	public class NicoTitleParseResult {
+
+		public NicoTitleParseResult(string title)
+			: this(title, new string[] {}, SongType.Original) {}
+
+		public NicoTitleParseResult(string title, string[] artistNames, SongType songType) {
+			ArtistNames = artistNames;
+			Title = title;
+			SongType = songType;
+		}
+
+		public string[] ArtistNames { get; set; }
+
+		public SongType SongType { get; set; }
+
+		public string Title { get; set; }
+
+	}
+}
