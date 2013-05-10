fb9aa2004d19e19d4adb6f184a3275d41b0bbf40
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-05-10 20:59:03 +0000

    Added selection for album view mode

---
+++ VocaDb/VocaDbWeb/Helpers/Support/CSSClassBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace VocaDb.Web.Helpers.Support {
+
+	public class CSSClassBuilder<T> where T : class {
+
+		private readonly T model;
+		private readonly StringBuilder str;
+
+		public CSSClassBuilder(T model, string initial) {
+			this.model = model;
+			str = new StringBuilder(initial);
+		}
+
+		public CSSClassBuilder<T> If(Func<T, bool> func, string className) {
+
+			if (func(model))
+				str.Append(" " + className);
+
+			return this;
+
+		}
+
+		public override string ToString() {
+			return str.ToString();
+		}
+
+	}
+}
