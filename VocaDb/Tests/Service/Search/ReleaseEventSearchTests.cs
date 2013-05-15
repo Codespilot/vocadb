ac6bc5b682b51bf1c163c14a0fd8aedac8445192
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-05-15 18:34:07 +0000

    Added tests for ReleaseEventSearch

---
+++ VocaDb/Tests/Service/Search/ReleaseEventSearchTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaDb.Model.DataContracts.ReleaseEvents;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Service.Search;
+using VocaDb.Tests.Mocks;
+
+namespace VocaDb.Tests.Service.Search {
+
+	/// <summary>
+	/// Tests for <see cref="ReleaseEventSearch"/>.
+	/// </summary>
+	[TestClass]
+	public class ReleaseEventSearchTests {
+
+		private ReleaseEvent eventInSeries;
+		private ReleaseEvent unsortedEvent;
+		private QuerySourceList querySource;
+		private ReleaseEventSeries series;
+		private ReleaseEventSearch target;
+
+		[TestInitialize]
+		public void SetUp() {
+
+			querySource = new QuerySourceList();
+
+			target = new ReleaseEventSearch(querySource);
+
+			series = new ReleaseEventSeries("Comiket", string.Empty, new[] { "C", "Comic Market" });
+
+			eventInSeries = new ReleaseEvent(string.Empty, null, series, 84);
+			querySource.Add(eventInSeries);
+
+			unsortedEvent = new ReleaseEvent(string.Empty, null, "Vocaloid Festa");
+			querySource.Add(unsortedEvent);
+
+			series.Events.Add(eventInSeries);
+			querySource.Add(series);
+
+		}
+
+		private ReleaseEventFindResultContract Find(string query) {
+			return target.Find(query);
+		}
+
+		/// <summary>
+		/// Find by combined series and event.
+		/// </summary>
+		[TestMethod]
+		public void FindSeriesAndEvent() {
+
+			var result = Find("Comiket 84");
+
+			Assert.IsNotNull(result, "Result");
+			Assert.AreEqual("Comiket 84", result.EventName, "EventName");
+
+		}
+
+		/// <summary>
+		/// Find by series, unknown event.
+		/// </summary>
+		[TestMethod]
+		public void FindNewEventEventInSeriesExact() {
+
+			var result = Find("Comiket 85");
+
+			Assert.IsNotNull(result, "Result");
+			Assert.IsNotNull(result.Series, "Series");
+			Assert.AreEqual("Comiket", result.Series.Name, "Series");
+			Assert.AreEqual(85, result.SeriesNumber, "SeriesNumber");
+
+		}
+
+		/// <summary>
+		/// Find by series, unknown event.
+		/// </summary>
+		[TestMethod]
+		public void FindUnsortedEvent() {
+
+			var result = Find("Vocaloid Festa");
+
+			Assert.IsNotNull(result, "Result");
+			Assert.AreEqual("Vocaloid Festa", result.EventName, "EventName");
+
+		}
+
+		/// <summary>
+		/// Doesn't match any series.
+		/// </summary>
+		[TestMethod]
+		public void FindNothing() {
+
+			var result = Find("Does not exist");
+
+			Assert.IsNotNull(result, "Result");
+			Assert.AreEqual(null, result.EventName, "EventName");
+
+		}
+
+	}
+
+}
---
+++ VocaDb/VocaDbModel/Service/Search/ReleaseEventSearch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using VocaDb.Model.DataContracts.ReleaseEvents;
+using VocaDb.Model.Domain.Albums;
+
+namespace VocaDb.Model.Service.Search {
+
+	public class ReleaseEventSearch {
+
+		private static readonly Regex eventNameRegex = new Regex(@"(.[^\d]+)(\d+)");
+
+		private readonly IQuerySource querySource;
+
+		private IQueryable<T> Query<T>() {
+			return querySource.Query<T>();
+		}
+
+		public ReleaseEventSearch(IQuerySource querySource) {
+			this.querySource = querySource;
+		}
+
+		public ReleaseEventFindResultContract Find(string query) {
+
+			// Attempt to match exact name
+			var ev = Query<ReleaseEvent>().FirstOrDefault(e => e.Name == query);
+
+			if (ev != null)
+				return new ReleaseEventFindResultContract(ev);
+
+			var match = eventNameRegex.Match(query);
+
+			if (match.Success) {
+
+				var seriesName = match.Groups[1].Value.Trim();
+				var seriesNumber = Convert.ToInt32(match.Groups[2].Value);
+
+				// Attempt to match series + series number
+				var results = Query<ReleaseEvent>().Where(e => e.SeriesNumber == seriesNumber && (seriesName.Contains(e.Series.Name) || e.Series.Name.Contains(seriesName)
+					|| e.Series.Aliases.Any(a => seriesName.Contains(a.Name) || a.Name.Contains(seriesName)))).ToArray();
+
+				if (results.Length > 1)
+					return new ReleaseEventFindResultContract();
+
+				if (results.Length == 1)
+					return new ReleaseEventFindResultContract(results[0]);
+
+				// Attempt to match just the series
+				var series = Query<ReleaseEventSeries>().FirstOrDefault(s => seriesName.Contains(s.Name) || s.Name.Contains(seriesName) || s.Aliases.Any(a => seriesName.Contains(a.Name) || a.Name.Contains(seriesName)));
+
+				if (series != null)
+					return new ReleaseEventFindResultContract(series, seriesNumber, query);
+
+			}
+
+			var events = Query<ReleaseEvent>().Where(e => query.Contains(e.Name) || e.Name.Contains(query)).Take(2).ToArray();
+
+			if (events.Length != 1) {
+				return new ReleaseEventFindResultContract();
+			}
+
+			return new ReleaseEventFindResultContract(events[0]);
+
+		}
+
+	}
+
+}
