9c9f2d2b30fc4297fe8104e4f4fa8929f4a2e667
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-05-28 21:06:30 +0000

    Archived versions management improvements and comments

--- VocaDb/VocaDbModel/DataContracts/Artists/ArchivedArtistVersionDetailsContract.cs
+++ VocaDb/VocaDbModel/DataContracts/Artists/ArchivedArtistVersionDetailsContract.cs
@@ -11,14 +11,16 @@ namespace VocaDb.Model.DataContracts.Artists {
 		public ArchivedArtistVersionDetailsContract(ArchivedArtistVersion archived, ArchivedArtistVersion comparedVersion,
 			ContentLanguagePreference languagePreference) {
 
+			ParamIs.NotNull(() => archived);
+
 			ArchivedVersion = new ArchivedArtistVersionContract(archived);
 			Artist = new ArtistContract(archived.Artist, languagePreference);
 			ComparedVersion = comparedVersion != null ? new ArchivedArtistVersionContract(comparedVersion) : null;
 			ComparedVersionId = comparedVersion != null ? comparedVersion.Id : 0;
 			Name = Artist.Name;
 
-			ComparableVersions = archived.Artist.ArchivedVersionsManager.Versions
-				.Where(v => v != archived)
+			ComparableVersions = archived.Artist.ArchivedVersionsManager
+				.GetPreviousVersions(archived)
 				.Select(a => new ArchivedObjectVersionContract(a))
 				.ToArray();
 
