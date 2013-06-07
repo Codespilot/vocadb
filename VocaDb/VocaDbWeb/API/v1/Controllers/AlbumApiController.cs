63200c4aef3fc14667fefe36fb570f2a282e6676
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-06-07 18:41:16 +0000

    Added tracks to Album API

--- VocaDb/VocaDbWeb/API/v1/Controllers/AlbumApiController.cs
+++ VocaDb/VocaDbWeb/API/v1/Controllers/AlbumApiController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using VocaDb.Model.DataContracts.Albums;
+using VocaDb.Model.DataContracts.Songs;
 using VocaDb.Model.Domain.Albums;
 using VocaDb.Model.Service;
 using VocaDb.Model.Service.Search.AlbumSearch;
@@ -17,7 +19,7 @@ namespace VocaDb.Web.API.v1.Controllers {
 
 		public ActionResult Details(int id, DataFormat format = DataFormat.Auto) {
 
-			var album = Service.GetAlbumDetails(id, null);
+			var album = Service.GetAlbum(id, a => new AlbumForApiContract(a, LoginManager.LanguagePreference));
 
 			return Object(album, format);
 
@@ -32,6 +34,14 @@ namespace VocaDb.Web.API.v1.Controllers {
 			var entries = Service.Find(a => new AlbumForApiContract(a, LoginManager.LanguagePreference), queryParams);
 
 			return Object(entries, format);
+
+		}
+
+		public ActionResult Tracks(int id, DataFormat format = DataFormat.Auto) {
+
+			var tracks = Service.GetAlbum(id, a => a.Songs.Select(s => new SongInAlbumContract(s, LoginManager.LanguagePreference)).ToArray());
+
+			return Object(tracks, format);
 
 		}
 
