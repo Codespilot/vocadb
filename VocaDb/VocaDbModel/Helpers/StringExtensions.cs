bdc9879091f467c325f83440065e0eea0c5bcdaa
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-06-09 19:11:57 +0000

    Metatags for artists as well, user authentication API

--- VocaDb/VocaDbModel/Helpers/StringExtensions.cs
+++ VocaDb/VocaDbModel/Helpers/StringExtensions.cs
@@ -10,5 +10,19 @@
 
 		}
 
+		/// <summary>
+		/// Truncates a string, adding ellipsis (three dots) at the end if the length exceeds a specific number.
+		/// </summary>
+		/// <param name="str">String to be processed. Cannot be null.</param>
+		/// <param name="length">Maximum length after which the string will be truncated.</param>
+		/// <returns>Truncated string with three dots at the end, if the string length exceeded the specified length, otherwise the original string.</returns>
+		public static string TruncateWithEllipsis(this string str, int length) {
+
+			ParamIs.NotNull(() => str);
+
+			return (str.Length > length ? str.Substring(0, length) + "..." : str);
+
+		}
+
 	}
 }
---
+++ VocaDb/VocaDbWeb/API/v1/Controllers/UserApiController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using VocaDb.Model.Service;
+using VocaDb.Web.Helpers;
+
+namespace VocaDb.Web.API.v1.Controllers {
+
+	public class UserApiController : Web.Controllers.ControllerBase {
+
+		private UserService Service {
+			get { return Services.Users; }
+		}
+
+		//[HttpPost]
+		public ActionResult Authenticate(string username, string accesskey) {
+			
+			var user = Service.CheckAccessWithKey(username, accesskey, WebHelper.GetRealHost(Request));
+
+			if (user == null) {
+				return Content("Username or password doesn't match");
+			} else {
+
+				FormsAuthentication.SetAuthCookie(user.Name, true);
+				return Content("OK");
+
+			}
+
+		}
+
+ 	}
+}
