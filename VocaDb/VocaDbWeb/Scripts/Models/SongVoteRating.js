d9e7790d4040da217894ab95310c6468f1a4d247
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-06-20 15:25:04 +0000

    converting rating to enum

--- VocaDb/VocaDbWeb/Scripts/Models/SongVoteRating.js
+++ VocaDb/VocaDbWeb/Scripts/Models/SongVoteRating.js
@@ -4,10 +4,21 @@ var vdb;
         (function (SongVoteRating) {
             SongVoteRating[SongVoteRating["Nothing"] = 0] = "Nothing";
             SongVoteRating[SongVoteRating["Like"] = 3] = "Like";
-
             SongVoteRating[SongVoteRating["Favorite"] = 5] = "Favorite";
         })(models.SongVoteRating || (models.SongVoteRating = {}));
         var SongVoteRating = models.SongVoteRating;
+
+        function parseSongVoteRating(rating) {
+            switch (rating) {
+                case "Nothing":
+                    return SongVoteRating.Nothing;
+                case "Like":
+                    return SongVoteRating.Like;
+                case "Favorite":
+                    return SongVoteRating.Favorite;
+            }
+        }
+        models.parseSongVoteRating = parseSongVoteRating;
     })(vdb.models || (vdb.models = {}));
     var models = vdb.models;
 })(vdb || (vdb = {}));
