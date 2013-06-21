f734defde1d6e480fedf803b11de3d560b0dcda8
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-06-21 10:16:49 +0000

    Hide PV preview by default

--- VocaDb/VocaDbWeb/Scripts/KnockoutExtensions/slideVisible.ts
+++ VocaDb/VocaDbWeb/Scripts/KnockoutExtensions/slideVisible.ts
@@ -8,8 +8,9 @@ interface KnockoutBindingHandlers {
 ko.bindingHandlers.slideVisible = {
     init: function (element, valueAccessor) {
         // Initially set the element to be instantly visible/hidden depending on the value
-        var value = valueAccessor();
-        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        if (value)
+            $(element).show();
     },
     update: function (element, valueAccessor, allBindingsAccessor) {
         // Whenever the value subsequently changes, slowly fade the element in or out
