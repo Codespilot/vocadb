fc89b855237f2526c460496fa7b66f3268554cba
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-07-31 19:53:54 +0000

    Moved route config to another file

---
+++ VocaDb/VocaDbWeb/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using VocaDb.Web.Code;
+
+namespace VocaDb.Web.App_Start {
+
+	public static class RouteConfig {
+
+		public static void RegisterRoutes(RouteCollection routes) {
+
+			// Ignored files
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("favicon.ico");
+
+			// Invalid routes
+			routes.MapRoute("AlbumDetailsError", "Album/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
+			routes.MapRoute("ArtistDetailsError", "Artist/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
+			routes.MapRoute("SongDetailsError", "Song/Details/{id}",
+				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
+
+			// Special routes
+			//routes.MapRoute("Search", "Search/{filter}", 
+			//	new { controller = "Home", action = "Search", filter = UrlParameter.Optional });	// Doesn't work well with certain chars
+			routes.MapRoute("Song", "S/{id}", new { controller = "Song", action = "Details" }, new { id = "[0-9]+" });
+			routes.MapRoute("User", "Profile/{id}", new { controller = "User", action = "Profile" });
+
+			// Default mapping
+			routes.MapRoute(
+				"Default", // Route name
+				"{controller}/{action}/{id}", // URL with parameters
+				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+			);
+
+		}
+
+	}
+
+}
--- VocaDb/VocaDbWeb/Global.asax.cs
+++ VocaDb/VocaDbWeb/Global.asax.cs
@@ -163,35 +163,6 @@ namespace VocaDb.Web {
 			filters.Add(new RestrictBlockedIPAttribute());
 		}
 
-		public static void RegisterRoutes(RouteCollection routes) {
-
-			// Ignored files
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-			routes.IgnoreRoute("favicon.ico");
-
-			// Invalid routes
-			routes.MapRoute("AlbumDetailsError", "Album/Details/{id}",
-				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
-			routes.MapRoute("ArtistDetailsError", "Artist/Details/{id}",
-				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
-			routes.MapRoute("SongDetailsError", "Song/Details/{id}",
-				new { controller = "Error", action = "NotFound" }, new { id = new IdNotNumberConstraint() });
-
-			// Special routes
-			//routes.MapRoute("Search", "Search/{filter}", 
-			//	new { controller = "Home", action = "Search", filter = UrlParameter.Optional });	// Doesn't work well with certain chars
-			routes.MapRoute("Song", "S/{id}", new { controller = "Song", action = "Details" }, new { id = "[0-9]+" });
-			routes.MapRoute("User", "Profile/{id}", new { controller = "User", action = "Profile" });
-
-			// Default mapping
-			routes.MapRoute(
-				"Default", // Route name
-				"{controller}/{action}/{id}", // URL with parameters
-				new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-			);
-
-		}
-
 		protected void Application_Start() {
 
 			log.Info("Web application starting.");
@@ -199,7 +170,7 @@ namespace VocaDb.Web {
 			AreaRegistration.RegisterAllAreas();
 
 			RegisterGlobalFilters(GlobalFilters.Filters);
-			RegisterRoutes(RouteTable.Routes);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 
 			log.Debug("Web application started successfully.");
