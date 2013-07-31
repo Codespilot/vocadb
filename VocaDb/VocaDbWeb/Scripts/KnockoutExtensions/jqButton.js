6379b56acd3978f5c0b8bbcbf6382884d1814ae5
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-07-31 18:34:54 +0000

    Edited jQuery UI buttons on song details page. Added jqButton binding.

---
+++ VocaDb/VocaDbWeb/Scripts/KnockoutExtensions/jqButton.js
@@ -0,0 +1,7 @@
+ko.bindingHandlers.jqButton = {
+    init: function (element, valueAccessor) {
+        var params = ko.utils.unwrapObservable(valueAccessor());
+
+        $(element).button({ disabled: params.disabled, icons: { primary: params.icon } });
+    }
+};
