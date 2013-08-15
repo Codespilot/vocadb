e052ab3aa7dc45626ffbd250d25d6fbe5a9f9470
riipah <riipah@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-08-15 18:52:30 +0000

    129: Fix loading more entries

--- VocaDb/VocaDbWeb/Models/Admin/ViewAuditLogModel.cs
+++ VocaDb/VocaDbWeb/Models/Admin/ViewAuditLogModel.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using VocaDb.Model;
 using VocaDb.Model.Service;
 using VocaDb.Web.Helpers;
 
 namespace VocaDb.Web.Models.Admin {
 
+	[JsonObject(MemberSerialization.OptIn)]
 	public class ViewAuditLogModel {
 
 		public ViewAuditLogModel() {
@@ -19,12 +21,16 @@ namespace VocaDb.Web.Models.Admin {
 
 		}
 
+		[JsonProperty]
 		public string ExcludeUsers { get; set; }
 
+		[JsonProperty]
 		public string Filter { get; set; }
 
+		[JsonProperty]
 		public AuditLogUserGroupFilter GroupId { get; set; }
 
+		[JsonProperty]
 		public bool OnlyNewUsers { get; set; }
 
 		public KeyValuePair<AuditLogUserGroupFilter, string>[] UserGroups { get; set; }
