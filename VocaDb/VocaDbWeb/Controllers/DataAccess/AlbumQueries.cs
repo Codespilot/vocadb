bbe95bbc6266b88be23f3f54e4477c8417bf0f34
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-08-28 20:15:39 +0000

    67: Related albums query

---
+++ VocaDb/VocaDbWeb/Controllers/DataAccess/AlbumQueries.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using VocaDb.Model.DataContracts.Albums;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Domain.Security;
+using VocaDb.Model.Service.Queries;
+using VocaDb.Model.Service.Repositories;
+
+namespace VocaDb.Web.Controllers.DataAccess {
+
+	/// <summary>
+	/// Database queries related to <see cref="Album"/>.
+	/// </summary>
+	public class AlbumQueries {
+
+		private readonly IUserPermissionContext permissionContext;
+		private readonly IAlbumRepository repository;
+
+		public AlbumQueries(IAlbumRepository repository, IUserPermissionContext permissionContext) {
+			this.repository = repository;
+			this.permissionContext = permissionContext;
+		}
+
+		public AlbumContract[] GetRelatedAlbums(int albumId) {
+
+			return repository.HandleQuery(ctx => {
+
+				var album = ctx.Load(albumId);
+				var q = new RelatedAlbumsQuery(ctx);
+				var albums = q.GetRelatedAlbums(album);
+
+				return albums.ArtistMatches
+					.Select(a => new AlbumContract(a, permissionContext.LanguagePreference))
+					.OrderBy(a => a.Name)
+					.Concat(albums.TagMatches
+						.Select(a => new AlbumContract(a, permissionContext.LanguagePreference))
+						.OrderBy(a => a.Name))
+					.ToArray();
+
+			});
+
+		}
+
+	}
+}
