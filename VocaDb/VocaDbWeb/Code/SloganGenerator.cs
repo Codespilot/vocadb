a34344e034ebfa5c616d72eabe6da14d193d822c
riipah <riipah@gmail.com>
2013-08-30 21:53:17 +0000

    opengraph tags for songlists

--- VocaDb/VocaDbWeb/Code/SloganGenerator.cs
+++ VocaDb/VocaDbWeb/Code/SloganGenerator.cs
@@ -4,13 +4,14 @@ namespace VocaDb.Web.Code {
 
 	public class SloganGenerator {
 
-		private static readonly string[] slogans = { "Telling you who's whoo.", "1st place to check." };
+		private static readonly string[] slogans = { "Telling you who's whoo.", "1st place to check.", "We got APIs and ApiMikus." };
 
 		public static string Generate() {
 
 			var result = slogans[new Random().Next(slogans.Length)];
 
 			return result;
+
 		}
 
 	}
