43eef6db172a80c59bfb374e9cc571a11b79d067
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-09-30 18:01:25 +0000

    Some cleanup

--- VocaDb/VocaDbModel/Service/VideoServices/NicoParser.cs
+++ VocaDb/VocaDbModel/Service/VideoServices/NicoParser.cs
@@ -57,7 +57,7 @@ namespace VocaDb.Model.Service.VideoServices {
 			WebResponse response;
 
 			try {
-				response = request.GetResponse();
+				response = request.GetResponse();	// Closed below
 			} catch (WebException x) {
 				log.WarnException("Unable to get response for user name", x);
 				return null;
@@ -120,38 +120,6 @@ namespace VocaDb.Model.Service.VideoServices {
 
 		}
 
-		public static VideoTitleParseResult GetTitleHtml(string id) {
-
-			var url = string.Format("http://nicovideo.jp/watch/{0}", id);
-
-			string videoTitle = null;
-			var request = WebRequest.Create(url);
-			WebResponse response;
-
-			try {
-				response = request.GetResponse();
-			} catch (WebException x) {
-				return VideoTitleParseResult.CreateError("NicoVideo (error): " + x.Message);
-			}
-
-			var enc = GetEncoding(response.Headers[HttpResponseHeader.ContentEncoding]);
-
-			try {
-				using (var stream = response.GetResponseStream()) {
-					videoTitle = GetVideoTitle(stream, enc);
-				}
-			} finally {
-				response.Close();
-			}
-
-			if (!string.IsNullOrEmpty(videoTitle)) {
-				return VideoTitleParseResult.CreateSuccess(videoTitle, null, null);
-			} else {
-				return VideoTitleParseResult.CreateError("Title element not found");
-			}
-
-		}
-
 		private static Encoding GetEncoding(string encodingStr) {
 
 			if (string.IsNullOrEmpty(encodingStr))
@@ -162,21 +130,6 @@ namespace VocaDb.Model.Service.VideoServices {
 			} catch (ArgumentException) {
 				return Encoding.UTF8;
 			}
-
-		}
-
-		private static string GetVideoTitle(Stream htmlStream, Encoding encoding) {
-
-			var doc = new HtmlDocument();
-			doc.Load(htmlStream, encoding);
-
-			// Video title element (could use page title as well but this is more reliable)
-			var titleElem = doc.DocumentNode.SelectSingleNode("//div[@id = 'PAGEBODY']/div/div/div/div/h1");
-			//var titleElem = doc.DocumentNode.SelectSingleNode("//p[@class = 'video_title']");
-
-			var titleText = (titleElem != null ? titleElem.InnerText : null);
-
-			return (titleText != null ? HtmlEntity.DeEntitize(titleText) : null);
 
 		}
 
