a3061f6a5e44e8ebf2acd4979571ec8bb8fa6790
riipah <riipah@gmail.com>
2013-10-01 20:07:36 +0000

    Get banners from static website. Some DataContract decorations and comments

--- VocaDb/VocaDbModel/DataContracts/EntryPictureFileContract.cs
+++ VocaDb/VocaDbModel/DataContracts/EntryPictureFileContract.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO;
+using System.Runtime.Serialization;
 using VocaDb.Model.Domain;
 
 namespace VocaDb.Model.DataContracts {
 
+	[DataContract(Namespace = Schemas.VocaDb)]
 	public class EntryPictureFileContract {
 
 		public EntryPictureFileContract() { }
@@ -19,18 +21,24 @@ namespace VocaDb.Model.DataContracts {
 
 		}
 
-		public int ContentLength { get; set; }
-
+		[DataMember]
 		public EntryType EntryType { get; set; }
 
+		[DataMember]
 		public string FileName { get; set; }
 
+		[DataMember]
 		public int Id { get; set; }
 
+		[DataMember]
 		public string Mime { get; set; }
 
+		[DataMember]
 		public string Name { get; set; }
 
+		/// <summary>
+		/// File data stream. Only used for uploads.
+		/// </summary>
 		public Stream UploadedFile { get; set; }
 
 		public EntryPictureFileContract NullToEmpty() {
