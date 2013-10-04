e7d39fd64fcebe18219c37aeed9b2af9acf18568
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-10-04 18:36:02 +0000

    More tests

---
+++ VocaDb/Tests/Web/Controllers/DataAccess/AlbumQueriesTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaDb.Model.DataContracts.Users;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Domain.Globalization;
+using VocaDb.Model.Domain.Users;
+using VocaDb.Tests.TestSupport;
+using VocaDb.Web.Code;
+using VocaDb.Web.Controllers.DataAccess;
+
+namespace VocaDb.Tests.Web.Controllers.DataAccess {
+
+	/// <summary>
+	/// Tests for <see cref="AlbumQueries"/>.
+	/// </summary>
+	[TestClass]
+	public class AlbumQueriesTests {
+
+		private Album album;
+		private FakeAlbumRepository repository;
+		private AlbumQueries queries;
+		private User user;
+
+		[TestInitialize]
+		public void SetUp() {
+			
+			album = new Album(TranslatedString.Create("Synthesis")) { Id = 39 };
+			repository = new FakeAlbumRepository(album);
+			user = new User { Name = "Miku", GroupId = UserGroupId.Regular, Id = 1 };
+			repository.Add(user);
+
+			var permissionContext = new FakePermissionContext(new UserWithPermissionsContract(user, ContentLanguagePreference.Default));
+			var entryLinkFactory = new EntryAnchorFactory("http://test.vocadb.net");
+
+			queries = new AlbumQueries(repository, permissionContext, entryLinkFactory);
+
+		}
+
+		[TestMethod]
+		public void CreateComment() {
+
+			var result = queries.CreateComment(39, "Hello world");
+			Assert.IsNotNull(result, "Result");
+
+			var comment = repository.List<AlbumComment>().FirstOrDefault();
+			Assert.IsNotNull(comment, "Comment was saved");
+			Assert.AreEqual(user, comment.Author, "Author");
+			Assert.AreEqual(album, comment.Album, "Album");
+			Assert.AreEqual("Hello world", comment.Message, "Comment message");
+
+		}
+
+	}
+
+}
