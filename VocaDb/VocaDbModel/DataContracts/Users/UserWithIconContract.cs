5aecb946ddfccfdb250d0fbaeba64d8443feb58a
riipah <riipah@gmail.com>
2013-10-06 11:39:37 +0000

    Viewmodel for messages

---
+++ VocaDb/VocaDbModel/DataContracts/Users/UserWithIconContract.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+using VocaDb.Model.Domain.Users;
+
+namespace VocaDb.Model.DataContracts.Users {
+
+	/// <summary>
+	/// User with profile icon URL.
+	/// Contains no sensitive information.
+	/// </summary>
+	[DataContract(Namespace = Schemas.VocaDb)]
+	public class UserWithIconContract : UserBaseContract, IUserWithIcon {
+
+		public UserWithIconContract() { }
+
+		public UserWithIconContract(User user, string iconUrl)
+			: base(user) {
+
+			IconUrl = iconUrl;
+
+		}
+
+		public UserWithIconContract(User user, IUserIconFactory iconFactory)
+			: this(user, iconFactory.GetIconUrl(user)) {}
+
+		[DataMember]
+		public string IconUrl { get; set; }
+
+	}
+
+}
