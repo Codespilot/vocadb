0ce6fa5ea3b9f87e788c4ac97cfbfb1dd1130254
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-11-18 17:38:23 +0000

    Small changes

--- VocaDb/VocaDbWeb/Helpers/VocaUrlHelper.cs
+++ VocaDb/VocaDbWeb/Helpers/VocaUrlHelper.cs
@@ -10,16 +10,33 @@ namespace VocaDb.Web.Helpers {
 		// TODO: should support HTTPS
 		private static readonly Uri staticResourceBase = new Uri("http://static.vocadb.net", UriKind.Absolute);
 
+		/// <summary>
+		/// Gets an URL-friendly name from any entry name.
+		/// The processed name can be used as user-friendly part of an URL.
+		/// 
+		/// English or Romanized name is preferred.
+		/// </summary>
 		public static string GetUrlFriendlyName(TranslatedString names) {
 			return GetUrlFriendlyName(names.GetBestMatch(ContentLanguagePreference.English));
 		}
 
+		/// <summary>
+		/// Gets an URL-friendly name from any entry name.
+		/// The processed name can be used as user-friendly part of an URL.
+		/// 
+		/// - Spaces are replaced with dashes '-'.
+		/// - All non-ASCII characters except digits, underscore '_' and dash '-' are removed.
+		/// - Name is truncated to 30 characters.
+		/// - Name is lowercased.
+		/// </summary>
+		/// <param name="name">Entry name, for example "Hatsune Miku". Can be null or empty.</param>
+		/// <returns>Processed name, for example "hatsune-miku". Can be empty. Cannot be null.</returns>
 		public static string GetUrlFriendlyName(string name) {
 
 			if (string.IsNullOrEmpty(name))
 				return string.Empty;
 
-			var cleanedName = Regex.Replace(name.Replace(' ', '-'), @"[^a-zA-Z_-]", string.Empty);
+			var cleanedName = Regex.Replace(name.Replace(' ', '-'), @"[^a-zA-Z0-9_-]", string.Empty);
 			return cleanedName.Truncate(30).ToLowerInvariant();
 
 		}
