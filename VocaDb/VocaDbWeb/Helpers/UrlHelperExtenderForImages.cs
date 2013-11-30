48f84d000000543f1a798e177b77c28998e0c314
riipah <riipah@gmail.com>
2013-11-30 17:58:42 +0000

    Thumbnails use static domain as well

--- VocaDb/VocaDbWeb/Helpers/UrlHelperExtenderForImages.cs
+++ VocaDb/VocaDbWeb/Helpers/UrlHelperExtenderForImages.cs
@@ -1,6 +1,7 @@
 ﻿using System.Web.Mvc;
 using VocaDb.Model.Domain;
 using VocaDb.Model.Domain.Images;
+using VocaDb.Model.Utils;
 
 namespace VocaDb.Web.Helpers {
 
@@ -27,29 +28,40 @@ namespace VocaDb.Web.Helpers {
 		/// <param name="urlHelper">URL helper. Cannot be null.</param>
 		/// <param name="imageInfo">Image information. Cannot be null.</param>
 		/// <param name="size">Requested image size.</param>
+		/// <param name="fullUrl">
+		/// Whether the URL should always include the hostname and application path root.
+		/// If this is false (default), the URL maybe either full (such as http://vocadb.net/Album/CoverPicture/123)
+		/// or relative (such as /Album/CoverPicture/123).
+		/// Usually this should be set to true if the image is to be referred from another domain.
+		/// </param>
 		/// <returns>URL to the image thumbnail.</returns>
-		public static string ImageThumb(this UrlHelper urlHelper, IEntryImageInformation imageInfo, ImageSize size) {
+		public static string ImageThumb(this UrlHelper urlHelper, IEntryImageInformation imageInfo, ImageSize size, bool fullUrl = false) {
 			
 			if (imageInfo == null)
 				return null;
 
 			var shouldExist = ShouldExist(imageInfo);
+			string dynamicUrl = null;
 
 			// Use MVC dynamic actions when requesting original or an image that doesn't exist on disk.
 			if (imageInfo.EntryType == EntryType.Album) {
 
 				if (size == ImageSize.Original)
-					return urlHelper.Action("CoverPicture", "Album", new { id = imageInfo.Id, v = imageInfo.Version });
+					dynamicUrl = urlHelper.Action("CoverPicture", "Album", new { id = imageInfo.Id, v = imageInfo.Version });
 				else if (shouldExist && !imagePersister.HasImage(imageInfo, size))
-					return urlHelper.Action("CoverPictureThumb", "Album", new { id = imageInfo.Id, v = imageInfo.Version });
+					dynamicUrl = urlHelper.Action("CoverPictureThumb", "Album", new { id = imageInfo.Id, v = imageInfo.Version });
 
 			} else if (imageInfo.EntryType == EntryType.Artist) {
 				
 				if (size == ImageSize.Original)
-					return urlHelper.Action("Picture", "Artist", new { id = imageInfo.Id, v = imageInfo.Version });
+					dynamicUrl = urlHelper.Action("Picture", "Artist", new { id = imageInfo.Id, v = imageInfo.Version });
 				else if (shouldExist && !imagePersister.HasImage(imageInfo, size))
-					return urlHelper.Action("PictureThumb", "Artist", new { id = imageInfo.Id, v = imageInfo.Version });
+					dynamicUrl = urlHelper.Action("PictureThumb", "Artist", new { id = imageInfo.Id, v = imageInfo.Version });
 
+			}
+
+			if (dynamicUrl != null) {				
+				return (fullUrl ? AppConfig.HostAddress + dynamicUrl : dynamicUrl);
 			}
 
 			if (!shouldExist)
