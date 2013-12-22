5b3bfe46584ef1bc1c6109206eb4c91b09c123bc
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2013-12-22 18:50:13 +0000

    Changed markdown binding to knockout

--- VocaDb/VocaDbWeb/Helpers/KnockoutHtmlExtender.cs
+++ VocaDb/VocaDbWeb/Helpers/KnockoutHtmlExtender.cs
@@ -54,6 +54,49 @@ namespace VocaDb.Web.Helpers {
 		}
 
 		/// <summary>
+		/// Text area bound to knockout model.
+		/// </summary>
+		/// <typeparam name="TModel">Model type.</typeparam>
+		/// <typeparam name="TProperty">Property expression type.</typeparam>
+		/// <param name="htmlHelper">HTML helper. Cannot be null.</param>
+		/// <param name="expression">Property access expression. Cannot be null.</param>
+		/// <param name="binding">Knockout data-bind attribute. Cannot be null.</param>
+		/// <param name="cssClass">CSS class attribute. Can be null or empty, in which case this attribute is not specified.</param>
+		/// <param name="id">ID attribute. Can be null or empty, in which case this attribute is not specified.</param>
+		/// <param name="maxLength">Max length attribute. Can be null, in which case this attribute is not specified.</param>
+		/// <param name="rows">Rows attribute. Can be null, in which case this attribute is not specified.</param>
+		/// <param name="cols">Cols attribute. Can be null, in which case this attribute is not specified.</param>
+		/// <returns></returns>
+		public static MvcHtmlString TextAreaForKnockout<TModel, TProperty>(
+			this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, 
+			string binding, string cssClass = null, string id = null, int? maxLength = null, int? rows = null, int? cols = null) {
+
+			ParamIs.NotNull(() => htmlHelper);
+			ParamIs.NotNull(() => expression);
+			ParamIs.NotNull(() => binding);
+
+			var htmlAttributes = new Dictionary<string, object> { { "data-bind", binding } };
+
+			if (!string.IsNullOrEmpty(cssClass))
+				htmlAttributes.Add("class", cssClass);
+
+			if (!string.IsNullOrEmpty(id))
+				htmlAttributes.Add("id", id);
+
+			if (maxLength.HasValue)
+				htmlAttributes.Add("maxlength", maxLength.Value);
+
+			if (rows.HasValue)
+				htmlAttributes.Add("rows", rows.Value);
+
+			if (cols.HasValue)
+				htmlAttributes.Add("cols", cols.Value);
+
+			return htmlHelper.TextAreaFor(expression, htmlAttributes);
+
+		}
+
+		/// <summary>
 		/// Text box bound to knockout model.
 		/// </summary>
 		/// <typeparam name="TModel">Model type.</typeparam>
