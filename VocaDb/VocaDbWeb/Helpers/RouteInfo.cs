75f6eb64befeb4fd203af0163f7ac78fa5ecc401
riipah <riipah@gmail.com>
2014-01-05 22:45:43 +0000

    Fixed OEmbed and entry tooltip routing errors caused by Web API

--- VocaDb/VocaDbWeb/Helpers/RouteInfo.cs
+++ VocaDb/VocaDbWeb/Helpers/RouteInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
 using System.Web;
 using System.Web.Routing;
 
@@ -13,25 +12,37 @@ namespace VocaDb.Web.Helpers {
 			RouteData = data;
 		}
 
-		public RouteInfo(Uri uri, string applicationPath) {
-			RouteData = RouteTable.Routes.GetRouteData(new InternalHttpContext(uri, applicationPath));
+		public RouteInfo(Uri uri, string applicationPath, HttpContextBase context) {
+			RouteData = RouteTable.Routes.GetRouteData(new InternalHttpContext(uri, applicationPath, context));
 		}
 
 		private class InternalHttpContext : HttpContextBase {
+
+			private readonly HttpContextBase context;
 			private readonly HttpRequestBase _request;
 
-			public InternalHttpContext(Uri uri, string applicationPath) {
-				_request = new InternalRequestContext(uri, applicationPath);
+			public InternalHttpContext(Uri uri, string applicationPath, HttpContextBase contextBase) {
+				_request = new InternalRequestContext(uri, applicationPath, contextBase.Request);
+				context = contextBase;
+			}
+
+			public override IDictionary Items {
+				get { return context.Items; }
 			}
 
 			public override HttpRequestBase Request { get { return _request; } }
 		}
 
 		private class InternalRequestContext : HttpRequestBase {
+
+			private readonly string applicationPath;
+			private readonly HttpRequestBase request;
 			private readonly string _appRelativePath;
 			private readonly string _pathInfo;
 
-			public InternalRequestContext(Uri uri, string applicationPath) {
+			public InternalRequestContext(Uri uri, string applicationPath, HttpRequestBase request) {
+
+				this.applicationPath = applicationPath;
 				_pathInfo = uri.Query;
 
 				/*if (String.IsNullOrEmpty(applicationPath) || !uri.AbsolutePath.StartsWith(applicationPath, StringComparison.OrdinalIgnoreCase))
@@ -39,10 +50,22 @@ namespace VocaDb.Web.Helpers {
 				else
 					_appRelativePath = uri.AbsolutePath;*/
 				_appRelativePath = uri.AbsolutePath;
+				this.request = request;
 			}
 
 			public override string AppRelativeCurrentExecutionFilePath { get { return String.Concat("~", _appRelativePath); } }
 			public override string PathInfo { get { return _pathInfo; } }
+
+			public override string ApplicationPath {
+				get { return applicationPath; }
+			}
+
+			public override string HttpMethod {
+				get {
+					return request.HttpMethod;
+				}
+			}
+
 		}
 	}  
 
