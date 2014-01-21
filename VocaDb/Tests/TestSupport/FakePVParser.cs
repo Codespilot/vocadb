1ff9017d847cf49ab847612851834b125ccd2cf8
riipah <riipah@gmail.com>
2014-01-21 19:32:40 +0000

    Added comments, changed PV parsing a bit.

--- VocaDb/Tests/TestSupport/FakePVParser.cs
+++ VocaDb/Tests/TestSupport/FakePVParser.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using VocaDb.Model.Domain;
 using VocaDb.Model.Service.VideoServices;
 
 namespace VocaDb.Tests.TestSupport {
 
 	public class FakePVParser : IPVParser {
 
-		public Func<string, VideoUrlParseResult> ResultFunc { get; set; }
+		public Func<string, bool, VideoUrlParseResult> ResultFunc { get; set; }
+
+		public Dictionary<string, VideoUrlParseResult> MatchedPVs { get; set; }
+
+		public FakePVParser() {
+			MatchedPVs = new Dictionary<string, VideoUrlParseResult>(StringComparer.InvariantCultureIgnoreCase);
+		}
 
 		public VideoUrlParseResult ParseByUrl(string url, bool getTitle) {
-			return ResultFunc(url);
+
+			if (!MatchedPVs.ContainsKey(url))
+				return ResultFunc(url, getTitle);
+
+			return MatchedPVs[url];
+
 		}
 
 	}
