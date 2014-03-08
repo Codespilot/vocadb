8c5556d149a6e6c834371060f75df5d94e09e43a
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-03-08 15:19:39 +0000

    song search page improvements

---
+++ VocaDb/VocaDbModel/Service/Search/SongSearch/ParsedSongQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace VocaDb.Model.Service.Search.SongSearch {
+
+	public class ParsedSongQuery {
+
+		public string ArtistTag { get; set; }
+
+		public string Name { get; set; }
+
+		public string NicoId { get; set; }
+
+		public string TagName { get; set; }
+
+		public bool HasNameQuery {
+			get {
+				return !string.IsNullOrEmpty(Name);
+			}
+		}
+
+	}
+
+}
