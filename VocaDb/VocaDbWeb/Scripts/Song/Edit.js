580b024d919adaa20b0b1db53be94b8d7c7fcf4c
riipah <riipah@gmail.com>
2014-03-10 18:36:55 +0000

    Updated searches

--- VocaDb/VocaDbWeb/Scripts/Song/Edit.js
+++ VocaDb/VocaDbWeb/Scripts/Song/Edit.js
@@ -97,7 +97,7 @@ function initPage(songId) {
 			createNewItem: vdb.resources.song.addExtraArtist,
 			createOptionFirstRow: function (item) { return item.Name + " (" + item.ArtistType + ")"; },
 			createOptionSecondRow: function (item) { return item.AdditionalNames; },
-			extraQueryParams: { artistTypes: "Vocaloid,UTAU,OtherVocalist,OtherVoiceSynthesizer,Producer,Circle,Unknown,Animator,Illustrator,Lyricist,OtherGroup,OtherIndividual" }
+			extraQueryParams: { artistTypes: "Vocaloid,UTAU,OtherVocalist,OtherVoiceSynthesizer,Producer,Circle,Unknown,Animator,Illustrator,Lyricist,OtherGroup,OtherIndividual,Utaite,Band" }
 		});
 
 	function artistAdded(row) {
