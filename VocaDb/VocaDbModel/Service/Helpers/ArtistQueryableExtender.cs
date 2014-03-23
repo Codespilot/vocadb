6ef444fd9af72818f7837d87816da21b0fbaff76
riipah <riipah@gmail.com>
2014-03-23 11:53:09 +0000

    Refactored artist search, added tag filter.

--- VocaDb/VocaDbModel/Service/Helpers/ArtistQueryableExtender.cs
+++ VocaDb/VocaDbModel/Service/Helpers/ArtistQueryableExtender.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using VocaDb.Model.Domain;
 using VocaDb.Model.Domain.Artists;
 using VocaDb.Model.Domain.Globalization;
 using VocaDb.Model.Helpers;
@@ -54,6 +55,117 @@ namespace VocaDb.Model.Service.Helpers {
 			}
 
 			return criteria;
+
+		}
+
+		public static IQueryable<Artist> WhereDraftsOnly(this IQueryable<Artist> query, bool draftsOnly) {
+
+			if (!draftsOnly)
+				return query;
+
+			return query.Where(a => a.Status == EntryStatus.Draft);
+
+		}
+
+		// TODO: should be combined with common name query somehow, but NH is making it hard
+		/// <summary>
+		/// Filters an artist query by a name query.
+		/// </summary>
+		/// <param name="query">Artist query. Cannot be null.</param>
+		/// <param name="nameFilter">Name filter string. If null or empty, no filtering is done.</param>
+		/// <param name="matchMode">Desired mode for matching names.</param>
+		/// <param name="words">
+		/// List of words for the words search mode. 
+		/// Can be null, in which case the words list will be parsed from <paramref name="nameFilter"/>.
+		/// </param>
+		/// <returns>Filtered query. Cannot be null.</returns>
+		public static IQueryable<Artist> WhereHasName(this IQueryable<Artist> query, string nameFilter, 
+			NameMatchMode matchMode, string[] words = null) {
+
+			if (string.IsNullOrEmpty(nameFilter))
+				return query;
+
+			switch (FindHelpers.GetMatchMode(nameFilter, matchMode)) {
+				case NameMatchMode.Exact:
+					return query.Where(m => m.Names.Names.Any(n => n.Value == nameFilter));
+
+				case NameMatchMode.Partial:
+					return query.Where(m => m.Names.Names.Any(n => n.Value.Contains(nameFilter)));
+
+				case NameMatchMode.StartsWith:
+					return query.Where(m => m.Names.Names.Any(n => n.Value.StartsWith(nameFilter)));
+
+				case NameMatchMode.Words:
+					words = words ?? FindHelpers.GetQueryWords(nameFilter);
+
+					switch (words.Length) {
+						case 1:
+							query = query.Where(q => q.Names.Names.Any(n => n.Value.Contains(words[0])));
+							break;
+						case 2:
+							query = query.Where(q => 
+								q.Names.Names.Any(n => n.Value.Contains(words[0]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[1]))
+							);
+							break;
+						case 3:
+							query = query.Where(q => 
+								q.Names.Names.Any(n => n.Value.Contains(words[0]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[1]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[2]))
+							);
+							break;
+						case 4:
+							query = query.Where(q => 
+								q.Names.Names.Any(n => n.Value.Contains(words[0]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[1]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[2]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[3]))
+							);
+							break;
+						case 5:
+							query = query.Where(q => 
+								q.Names.Names.Any(n => n.Value.Contains(words[0]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[1]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[2]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[3]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[4]))
+							);
+							break;
+						case 6:
+							query = query.Where(q => 
+								q.Names.Names.Any(n => n.Value.Contains(words[0]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[1]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[2]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[3]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[4]))
+								&& q.Names.Names.Any(n => n.Value.Contains(words[5]))
+							);
+							break;
+					}
+					return query;
+
+			}
+
+			return query;
+
+		}
+
+		public static IQueryable<Artist> WhereHasTag(this IQueryable<Artist> query, string tagName) {
+
+			if (string.IsNullOrEmpty(tagName))
+				return query;
+
+			return query.Where(s => s.Tags.Usages.Any(a => a.Tag.Name == tagName));
+
+		}
+
+		public static IQueryable<Artist> WhereHasType(this IQueryable<Artist> query, ArtistType[] artistTypes) {
+
+			if (!artistTypes.Any())
+				return query;
+
+			return query.Where(m => artistTypes.Contains(m.ArtistType));
 
 		}
 
