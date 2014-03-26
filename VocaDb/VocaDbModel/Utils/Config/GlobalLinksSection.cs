1f15f988dded2fe0bfdf56ad81c8491d24cb5d21
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-03-26 20:35:14 +0000

    Removed unnecessary config code

--- VocaDb/VocaDbModel/Utils/Config/GlobalLinksSection.cs
+++ VocaDb/VocaDbModel/Utils/Config/GlobalLinksSection.cs
@@ -4,69 +4,6 @@ using System.Xml;
 
 namespace VocaDb.Model.Utils.Config {
 
-	/*public class GlobalLinksSection : ConfigurationSectionGroup {
-
-		public GlobalLinksSection() {
-			BigBanners = new LinksSection();
-			SmallBanners = new LinksSection();
-			SocialLinks = new LinksSection();
-		}
-
-		[ConfigurationProperty("bigBanners")] 
-		public LinksSection BigBanners { get; private set; }
-
-		[ConfigurationProperty("smallBanners")] 
-		public LinksSection SmallBanners { get; private set; }
-
-		[ConfigurationProperty("socialLinks")] 
-		public LinksSection SocialLinks { get; private set; }
-
-	}
-
-	public class LinksSection : ConfigurationSection {
-
-		public LinkElement[] Links { get; set; }
-
-	}
-
-	public class LinkElement : ConfigurationElement {
-		
-		public string BannerImg {
-			get { return (string)this["bannerImg"]; }
-		}
-
-		public string Title {
-			get { return (string)this["title"]; }
-		}
-
-		public string Url {
-			get { return (string)this["url"]; }
-		}
-
-	}*/
-
-	/*public class GlobalLinksSection : ConfigurationSectionGroup {
-
-		public LinksSection BigBanners {
-			get {
-				return (LinksSection)this.Sections["bigBanners"];
-			}
-		}
-
-		public LinksSection SmallBanners {
-			get {
-				return (LinksSection)this.Sections["smallBanners"];
-			}
-		}
-
-		public LinksSection SocialLinks {
-			get {
-				return (LinksSection)this.Sections["socialLinks"];
-			}
-		}
-
-	}*/
-
 	public class GlobalLinksSection : ConfigurationSectionGroup {
 
 		public LinksSection BigBanners { get; set; }
@@ -110,21 +47,5 @@ namespace VocaDb.Model.Utils.Config {
 		public string Url { get; set; }
 
 	}
-
-	/*public class LinkElement : ConfigurationElement {
-		
-		public string BannerImg {
-			get { return (string)this["bannerImg"]; }
-		}
-
-		public string Title {
-			get { return (string)this["title"]; }
-		}
-
-		public string Url {
-			get { return (string)this["url"]; }
-		}
-
-	}*/
 
 }
