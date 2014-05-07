cc84dc6a7b35c40ea4730e4b0b2b516d78a50390
riipah <riipah@gmail.com>
2014-05-07 19:08:24 +0000

    Artist filtering for albums

--- VocaDb/VocaDbWeb/Scripts/Repositories/AlbumRepository.ts
+++ VocaDb/VocaDbWeb/Scripts/Repositories/AlbumRepository.ts
@@ -20,14 +20,18 @@ module vdb.repositories {
 
 		}
 
-		getList = (paging: dc.PagingProperties, query: string, sort: string, discTypes: string, tag: string, callback) => {
+		getList = (paging: dc.PagingProperties, query: string, sort: string, discTypes: string, tag: string,
+			artistId: number, artistParticipationStatus: string,
+			callback) => {
 
 			var url = vdb.functions.mergeUrls(this.baseUrl, "/api/albums");
 			var data = {
 				start: paging.start, getTotalCount: paging.getTotalCount, maxEntries: paging.maxEntries,
 				query: query, fields: "MainPicture", lang: 'English', nameMatchMode: 'Words', sort: sort,
 				discTypes: discTypes,
-				tag: tag
+				tag: tag,
+				artistId: artistId,
+				artistParticipationStatus: artistParticipationStatus
 			};
 
 			$.getJSON(url, data, callback);
