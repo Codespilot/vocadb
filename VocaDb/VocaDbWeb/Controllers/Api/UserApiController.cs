bc4d1c572e33182fc8b2c97c18f0d206594ff504
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-05-13 20:28:15 +0000

    Added JSONP callback, web API

---
+++ VocaDb/VocaDbWeb/Controllers/Api/UserApiController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using VocaDb.Model.Domain.Security;
+using VocaDb.Model.Domain.Songs;
+using VocaDb.Model.Service;
+
+namespace VocaDb.Web.Controllers.Api {
+
+	[RoutePrefix("api/users")]
+	public class UserApiController : ApiController {
+
+		private readonly IUserPermissionContext permissionContext;
+		private readonly UserService service;
+
+		public UserApiController(UserService service, IUserPermissionContext permissionContext) {
+			this.service = service;
+			this.permissionContext = permissionContext;
+		}
+
+		[Route("current/ratedSongs/{songId:int}")]
+		[Authorize]
+		public string PostRating(int songId, SongVoteRating rating) {
+			
+			service.UpdateSongRating(permissionContext.LoggedUserId, songId, rating);
+			return "OK";
+
+		}
+
+	}
+}
