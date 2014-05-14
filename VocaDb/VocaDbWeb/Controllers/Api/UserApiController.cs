e462cc7b1998b449312830ced5de6c22dc8d76b3
riipah <riipah@gmail.com>
2014-05-14 16:29:40 +0000

    Docs

--- VocaDb/VocaDbWeb/Controllers/Api/UserApiController.cs
+++ VocaDb/VocaDbWeb/Controllers/Api/UserApiController.cs
@@ -6,6 +6,9 @@ using VocaDb.Model.Service;
 
 namespace VocaDb.Web.Controllers.Api {
 
+	/// <summary>
+	/// API queries for users.
+	/// </summary>
 	[RoutePrefix("api/users")]
 	public class UserApiController : ApiController {
 
@@ -17,10 +20,19 @@ namespace VocaDb.Web.Controllers.Api {
 			this.permissionContext = permissionContext;
 		}
 
+		/// <summary>
+		/// Add or update rating for a specific song, for the currently logged in user.
+		/// If the user has already rated the song, any previous rating is replaced.
+		/// Authorization cookie must be included.
+		/// This API supports CORS.
+		/// </summary>
+		/// <param name="songId">ID of the song to be rated.</param>
+		/// <param name="rating">Rating to be given. Possible values are Nothing, Like, Favorite.</param>
+		/// <returns>The string "OK" if successful.</returns>
 		[Route("current/ratedSongs/{songId:int}")]
 		[Authorize]
 		[EnableCors(origins: "*", headers: "*", methods: "post", SupportsCredentials = true)]
-		public string PostRating(int songId, SongVoteRating rating) {
+		public string PostSongRating(int songId, SongVoteRating rating) {
 			
 			service.UpdateSongRating(permissionContext.LoggedUserId, songId, rating);
 			return "OK";
