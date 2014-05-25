4d98286a4796e3a3683dfa06d14fb6094e1bddbf
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-05-25 17:41:51 +0000

    Unit tests

---
+++ VocaDb/Tests/Web/Controllers/DataAccess/EntryQueriesTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaDb.Model.DataContracts.Api;
+using VocaDb.Model.Domain;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Domain.Artists;
+using VocaDb.Model.Domain.Globalization;
+using VocaDb.Model.Service;
+using VocaDb.Model.Service.Repositories;
+using VocaDb.Tests.TestData;
+using VocaDb.Tests.TestSupport;
+using VocaDb.Web.Controllers.DataAccess;
+
+namespace VocaDb.Tests.Web.Controllers.DataAccess {
+
+	[TestClass]
+	public class EntryQueriesTests {
+
+		private FakeAlbumRepository repository;
+		private EntryQueries queries;
+
+		private EntryForApiContract AssertHasEntry(PartialFindResult<EntryForApiContract> result, string name, EntryType entryType) {
+			
+			var entry = result.Items.FirstOrDefault(a => string.Equals(a.DefaultName, name, StringComparison.InvariantCultureIgnoreCase)
+				&& a.EntryType == entryType);
+
+			Assert.IsNotNull(entry, "Entry found");
+			return entry;
+
+		}
+
+		private PartialFindResult<EntryForApiContract> CallGetList(string query, 
+			string tag = null,
+			EntryStatus? status = null,
+			int start = 0, int maxResults = 10, bool getTotalCount = true,
+			NameMatchMode nameMatchMode = NameMatchMode.Words,
+			EntryOptionalFields fields = EntryOptionalFields.None,
+			ContentLanguagePreference lang = ContentLanguagePreference.Default,
+			bool ssl = false) {
+			
+			return queries.GetList(query, tag, status, start, maxResults, getTotalCount, nameMatchMode, fields, lang, ssl);
+
+		}
+
+		[TestInitialize]
+		public void SetUp() {
+			
+			repository = new FakeAlbumRepository();
+			var permissionContext = new FakePermissionContext();
+			var thumbPersister = new InMemoryImagePersister();
+
+			queries = new EntryQueries(repository, permissionContext, thumbPersister);
+
+			var group = CreateEntry.Artist(ArtistType.OtherGroup, name: "1640mP");
+			var artist = CreateEntry.Producer(name: "40mP");
+			var artist2 = CreateEntry.Producer(name: "Tripshots");
+			var album = CreateEntry.Album(name: "40mP Piano Arrange Album");
+			var song = CreateEntry.Song(name: "Mosaik Role [40mP ver.]");
+
+			repository.Save(group, artist, artist2);
+			repository.Save(album);
+			repository.Save(song);
+
+		}
+
+		[TestMethod]
+		public void List() {
+			
+			var result = CallGetList(query: "40mP");
+
+			Assert.AreEqual(4, result.TotalCount, "TotalCount");
+			Assert.AreEqual(4, result.Items.Length, "Items.Length");
+
+			AssertHasEntry(result, "40mP", EntryType.Artist);
+			AssertHasEntry(result, "1640mP", EntryType.Artist);
+			AssertHasEntry(result, "40mP Piano Arrange Album", EntryType.Album);
+			AssertHasEntry(result, "Mosaik Role [40mP ver.]", EntryType.Song);
+
+		}
+
+		[TestMethod]
+		public void List_CanonizedArtistName() {
+			
+			var artist = CreateEntry.Producer(name: "nightmare-P");
+			repository.Save(artist);
+
+			var resultExact = CallGetList(query: "nightmare-P");
+			var resultVariant = CallGetList(query: "nightmareP");
+			var resultPartial = CallGetList(query: "nightmare");
+
+			AssertHasEntry(resultExact, "nightmare-P", EntryType.Artist);
+			AssertHasEntry(resultVariant, "nightmare-P", EntryType.Artist);
+			AssertHasEntry(resultPartial, "nightmare-P", EntryType.Artist);
+
+		}
+
+	}
+
+}
--- VocaDb/VocaDbWeb/Controllers/Api/EntryApiController.cs
+++ VocaDb/VocaDbWeb/Controllers/Api/EntryApiController.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Linq;
 using System.Web.Http;
 using VocaDb.Model.DataContracts.Api;
 using VocaDb.Model.Domain;
-using VocaDb.Model.Domain.Albums;
 using VocaDb.Model.Domain.Globalization;
-using VocaDb.Model.Domain.Images;
-using VocaDb.Model.Domain.Security;
-using VocaDb.Model.Domain.Songs;
-using VocaDb.Model.Helpers;
-using VocaDb.Model.Service.Helpers;
-using VocaDb.Model.Domain.Artists;
 using VocaDb.Model.Service;
-using VocaDb.Model.Service.Repositories;
+using VocaDb.Web.Controllers.DataAccess;
 using VocaDb.Web.Helpers;
 
 namespace VocaDb.Web.Controllers.Api {
@@ -26,18 +18,14 @@ namespace VocaDb.Web.Controllers.Api {
 		private const int absoluteMax = 50;
 		private const int defaultMax = 10;
 
-		private readonly IEntryThumbPersister entryThumbPersister;
-		private readonly IUserPermissionContext permissionContext;
-		private readonly IAlbumRepository repository;
+		private readonly EntryQueries queries;
 
 		private int GetMaxResults(int max) {
 			return Math.Min(max, absoluteMax);	
 		}
 
-		public EntryApiController(IAlbumRepository repository, IUserPermissionContext permissionContext, IEntryThumbPersister entryThumbPersister) {
-			this.repository = repository;
-			this.permissionContext = permissionContext;
-			this.entryThumbPersister = entryThumbPersister;
+		public EntryApiController(EntryQueries queries) {
+			this.queries = queries;
 		}
 
 		/// <summary>
@@ -67,103 +55,8 @@ namespace VocaDb.Web.Controllers.Api {
 			
 			var ssl = WebHelper.IsSSL(Request);
 			maxResults = GetMaxResults(maxResults);
-			query = FindHelpers.GetMatchModeAndQueryForSearch(query, ref nameMatchMode);
-			var canonized = ArtistHelper.GetCanonizedName(query);
 
-			return repository.HandleQuery(ctx => {
-
-				// Get all applicable names per entry type
-				var artistNames = ctx.OfType<Artist>().Query()
-					.Where(a => !a.Deleted)
-					.WhereHasName(canonized, nameMatchMode)
-					.WhereHasTag(tag)
-					.WhereStatusIs(status)
-					.OrderBy(ArtistSortRule.Name, lang)
-					.Take(start + maxResults)
-					.SelectEntryBase(lang, EntryType.Artist)
-					.ToArray();
-
-				var albumNames = ctx.OfType<Album>().Query()
-					.Where(a => !a.Deleted)
-					.WhereHasName(query, nameMatchMode)
-					.WhereHasTag(tag)
-					.WhereStatusIs(status)
-					.OrderBy(AlbumSortRule.Name, lang)
-					.Take(start + maxResults)
-					.SelectEntryBase(lang, EntryType.Album)
-					.ToArray();
-
-				var songNames = ctx.OfType<Song>().Query()
-					.Where(a => !a.Deleted)
-					.WhereHasName(query, nameMatchMode)
-					.WhereHasTag(tag)
-					.WhereStatusIs(status)
-					.OrderBy(SongSortRule.Name, lang)
-					.Take(start + maxResults)
-					.SelectEntryBase(lang, EntryType.Song)
-					.ToArray();
-
-				// Get page of combined names
-				var entryNames = artistNames.Concat(albumNames).Concat(songNames)
-					.OrderBy(e => e.DefaultName)
-					.Skip(start)
-					.Take(maxResults)
-					.ToArray();
-				
-				var artistIds = entryNames.Where(e => e.EntryType == EntryType.Artist).Select(a => a.Id).ToArray();
-				var albumIds = entryNames.Where(e => e.EntryType == EntryType.Album).Select(a => a.Id).ToArray();
-				var songIds = entryNames.Where(e => e.EntryType == EntryType.Song).Select(a => a.Id).ToArray();
-				var allIds = entryNames.Select(e => e.Id).ToArray();
-
-				// Get the actual entries in the page
-				var artists = artistIds.Any() ? ctx.OfType<Artist>().Query()
-					.Where(a => artistIds.Contains(a.Id))
-					.ToArray()
-					.Select(a => new EntryForApiContract(a, lang, entryThumbPersister, ssl, fields)) : new EntryForApiContract[0];
-
-				var albums = albumIds.Any() ? ctx.OfType<Album>().Query()
-					.Where(a => albumIds.Contains(a.Id))
-					.ToArray()
-					.Select(a => new EntryForApiContract(a, lang, entryThumbPersister, ssl, fields)) : new EntryForApiContract[0];
-
-				var songs = songIds.Any() ? ctx.OfType<Song>().Query()
-					.Where(a => songIds.Contains(a.Id))
-					.ToArray()
-					.Select(a => new EntryForApiContract(a, lang, fields)) : new EntryForApiContract[0];
-
-				// Merge and sort the final list
-				var entries = CollectionHelper.SortByIds(artists.Concat(albums).Concat(songs), allIds);
-
-				var count = 0;
-
-				if (getTotalCount) {
-					
-					count = 
-						ctx.OfType<Artist>().Query()
-							.Where(a => !a.Deleted)
-							.WhereHasName(canonized, nameMatchMode)
-							.WhereHasTag(tag)
-							.WhereStatusIs(status)
-							.Count() + 
-						ctx.OfType<Album>().Query()
-							.Where(a => !a.Deleted)
-							.WhereHasName(query, nameMatchMode)
-							.WhereHasTag(tag)
-							.WhereStatusIs(status)
-							.Count() +
-						ctx.OfType<Song>().Query()
-							.Where(a => !a.Deleted)
-							.WhereHasName(query, nameMatchMode)
-							.WhereHasTag(tag)
-							.WhereStatusIs(status)
-							.Count();
-
-
-				}
-
-				return new PartialFindResult<EntryForApiContract>(entries.ToArray(), count);
-
-			});
+			return queries.GetList(query, tag, status, start, maxResults, getTotalCount, nameMatchMode, fields, lang, ssl);
 
 		}
 
