3dd79110b7c97450869ee01d3c0c7d8aaac190b9
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-05-27 17:55:54 +0000

    Moved search

--- VocaDb/VocaDbWeb/Controllers/SearchController.cs
+++ VocaDb/VocaDbWeb/Controllers/SearchController.cs
@@ -1,16 +1,43 @@
 ﻿using System.Web.Mvc;
+using VocaDb.Model.Domain;
+using VocaDb.Model.Service;
 
 namespace VocaDb.Web.Controllers
 {
     public class SearchController : Controller
     {
-        //
-        // GET: /Search/
+        private readonly OtherService services;
 
-        public ActionResult Index(string filter)
-        {
-			return RedirectToAction("Search", "Home", new { filter });
-        }
+		public SearchController(OtherService services) {
+			this.services = services;
+		}
+
+		public ActionResult Index(string filter, EntryType searchType = EntryType.Undefined) {
+
+			filter = filter ?? string.Empty;
+			var result = services.Find(filter, 1, true);
+
+			if (result.OnlyOneItem) {
+
+				if (result.Albums.TotalCount == 1)
+					return RedirectToAction("Details", "Album", new { id = result.Albums.Items[0].Id });
+
+				if (result.Artists.TotalCount == 1)
+					return RedirectToAction("Details", "Artist", new { id = result.Artists.Items[0].Id });
+
+				if (result.Songs.TotalCount == 1)
+					return RedirectToAction("Details", "Song", new { id = result.Songs.Items[0].Id });
+
+				if (result.Tags.TotalCount == 1)
+					return RedirectToAction("Details", "Tag", new { id = result.Tags.Items[0].Name });
+
+			}
+
+			ViewBag.Query = filter;
+			ViewBag.SearchType = searchType;
+			return View();
+
+		}
 
     }
 }
