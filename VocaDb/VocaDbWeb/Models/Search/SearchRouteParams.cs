e8e06de7405fdf3414b0ee4d76a204c91cc1477e
riipah <riipah@gmail.com>
2014-05-28 20:52:26 +0000

    Changed the rest of the routes

---
+++ VocaDb/VocaDbWeb/Models/Search/SearchRouteParams.cs
@@ -0,0 +1,25 @@
+﻿using VocaDb.Model.Domain;
+using VocaDb.Model.Domain.Albums;
+using VocaDb.Model.Domain.Artists;
+
+namespace VocaDb.Web.Models.Search {
+
+	public class SearchRouteParams {
+
+		public int? artistId { get; set; }
+
+		public ArtistType? artistType { get; set; }
+
+		public DiscType? discType { get; set; }
+
+		public string filter { get; set; }
+
+		public EntryType? searchType { get; set; }
+
+		public object sort { get; set; }
+
+		public string tag { get; set; }
+
+	}
+
+}
