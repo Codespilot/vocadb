3e75f44f179ffe8ea17e6db056fdea9c09ec6a64
riipah <riipah@gmail.com>
2014-06-22 13:23:37 +0000

    Play-asia affiliate link generation

--- VocaDb/VocaDbModel/Service/ExtSites/AffiliateLinkGenerator.cs
+++ VocaDb/VocaDbModel/Service/ExtSites/AffiliateLinkGenerator.cs
@@ -1,4 +1,6 @@
-﻿using VocaDb.Model.Service.VideoServices;
+﻿using System;
+using System.Text.RegularExpressions;
+using VocaDb.Model.Service.VideoServices;
 
 namespace VocaDb.Model.Service.ExtSites {
 
@@ -10,12 +12,41 @@ namespace VocaDb.Model.Service.ExtSites {
 		private static readonly RegexLinkMatcher cdjRegex = new RegexLinkMatcher("http://www.cdjapan.co.jp/aff/click.cgi/PytJTGW7Lok/4412/A585851/detailview.html?{0}",
 			@"http://www.cdjapan\.co\.jp/detailview.html\?(KEY=\w+-\d+)");
 
-		public static string GenerateAffiliateLink(string url) {
+		private const string paAffId = "852809";
 
-			if (!cdjRegex.IsMatch(url))
+		private string ReplacePlayAsiaLink(string url) {
+			
+			if (url.Contains("www.play-asia.com/")) {
+
+				if (url.Contains("affiliate_id=")) {
+					
+					return Regex.Replace(url, @"affiliate_id=(\d+)", string.Format(@"affiliate_id={0}", paAffId));
+
+				} else if (url.Contains("?")) {
+					return string.Format("{0}&affiliate_id={1}", url, paAffId);
+				} else {
+					return string.Format("{0}?affiliate_id={1}", url, paAffId);
+				}
+
+			}
+
+			return url;
+
+		}
+
+		public string GenerateAffiliateLink(string url) {
+
+			if (string.IsNullOrEmpty(url))
 				return url;
 
-			return cdjRegex.MakeLinkFromUrl(url);
+			/*if (cdjRegex.IsMatch(url)) {
+				return cdjRegex.MakeLinkFromUrl(url);				
+			}*/
+
+			url = ReplacePlayAsiaLink(url);
+
+			return url;
+
 
 		}
 
