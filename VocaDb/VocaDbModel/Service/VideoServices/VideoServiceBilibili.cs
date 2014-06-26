7ab4b7296d5733d89ab575b2d064b9ad7c3a646e
riipah <riipah@gmail.com>
2014-06-26 21:00:38 +0000

    Added bilibili.com matcher

--- VocaDb/VocaDbModel/Service/VideoServices/VideoServiceBilibili.cs
+++ VocaDb/VocaDbModel/Service/VideoServices/VideoServiceBilibili.cs
@@ -16,7 +16,8 @@ namespace VocaDb.Model.Service.VideoServices {
 		private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
 		public static readonly RegexLinkMatcher[] Matchers =
-			new[] {
+			{
+				new RegexLinkMatcher("acg.tv/av{0}", @"www.bilibili.com/video/av(\d+)"),
 				new RegexLinkMatcher("acg.tv/av{0}", @"acg.tv/av(\d+)"),
 				new RegexLinkMatcher("acg.tv/av{0}", @"www.bilibili.tv/video/av(\d+)"),
 				new RegexLinkMatcher("acg.tv/av{0}", @"bilibili.kankanews.com/video/av(\d+)")
