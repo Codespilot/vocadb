e00fc1f5f8d9e04a07f2087616d4a8bf835e8885
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-10-30 17:31:33 +0000

    Hide chart if no data

--- VocaDb/VocaDbWeb/Scripts/KnockoutExtensions/Highcharts.ts
+++ VocaDb/VocaDbWeb/Scripts/KnockoutExtensions/Highcharts.ts
@@ -1,15 +1,20 @@
 ﻿/// <reference path="../typings/highcharts/highcharts.d.ts" />
 
 interface KnockoutBindingHandlers {
-	// Binding handler for jQuery focusout event.
 	highcharts: KnockoutBindingHandler;
 }
 
 ko.bindingHandlers.highcharts = {
 	init: (element: HTMLElement, valueAccessor) => {
+
 		var func = ko.utils.unwrapObservable<Function>(valueAccessor());
-		func((result: HighchartsOptions) => $(element).highcharts(result));
-		//var url = ko.utils.unwrapObservable<string>(valueAccessor());
-		//$.getJSON(url, (result: HighchartsOptions) => $(element).highcharts(result));
+
+		func((result: HighchartsOptions) => {
+			if (result && result.series && result.series.length && result.series[0].data && result.series[0].data.length)
+				$(element).highcharts(result);
+			else
+				$(element).hide();
+		});
+
 	}
 };
