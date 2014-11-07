bd651652f6b6fb17e2f0a9c5d3c5729b4cd2292a
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-11-07 20:20:53 +0000

    Removed unused code

--- VocaDb/VocaDbWeb/Controllers/TagController.cs
+++ VocaDb/VocaDbWeb/Controllers/TagController.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Web.Mvc;
-using MvcPaging;
-using VocaDb.Model.DataContracts.Albums;
-using VocaDb.Model.DataContracts.Artists;
-using VocaDb.Model.DataContracts.Songs;
 using VocaDb.Web.Controllers.DataAccess;
 using VocaDb.Web.Helpers;
 using VocaDb.Model.DataContracts;
 using VocaDb.Model.DataContracts.Tags;
 using VocaDb.Model.Domain.Tags;
 using VocaDb.Model.Service;
-using VocaDb.Web.Models.Shared;
 using VocaDb.Web.Models.Tag;
 using VocaDb.Web.Resources.Controllers;
 
@@ -29,44 +24,6 @@ namespace VocaDb.Web.Controllers
 
 			this.service = service;
 			this.queries = queries;
-
-		}
-
-		public ActionResult Albums(string id) {
-
-			return RedirectToActionPermanent("AlbumTagUsages", new { id });
-
-		}
-
-		public ActionResult AlbumTagUsages(string id, int? page) {
-
-			var pageIndex = (page - 1) ?? 0;
-			var result = Service.GetAlbums(id, pageIndex * entriesPerPage, entriesPerPage);
-			var data = new PagingData<AlbumTagUsageContract>(result.Items.ToPagedList(pageIndex, entriesPerPage, result.TotalCount), id, "AlbumTagUsages", "ui-tabs-2");
-
-			if (Request.IsAjaxRequest())
-				return PartialView("AlbumTagUsages", data);
-			else
-				return View("AlbumTagUsages", data);
-
-		}
-
-		public ActionResult ArtistTagUsages(string id, int? page) {
-
-			var pageIndex = (page - 1) ?? 0;
-			var result = Service.GetArtists(id, pageIndex * entriesPerPage, entriesPerPage);
-			var data = new PagingData<ArtistTagUsageContract>(result.Items.ToPagedList(pageIndex, entriesPerPage, result.TotalCount), id, "ArtistTagUsages", "ui-tabs-1");
-
-			if (Request.IsAjaxRequest())
-				return PartialView("ArtistTagUsages", data);
-			else
-				return View("ArtistTagUsages", data);
-
-		}
-
-		public ActionResult Artists(string id) {
-
-			return RedirectToActionPermanent("ArtistTagUsages", new { id });
 
 		}
 
@@ -171,25 +128,6 @@ namespace VocaDb.Web.Controllers
 			}
 
 			return View(tags);
-
-		}
-
-		public ActionResult Songs(string id) {
-
-			return RedirectToActionPermanent("SongTagUsages", new { id });
-
-		}
-
-		public ActionResult SongTagUsages(string id, int? page) {
-
-			var pageIndex = (page - 1) ?? 0;
-			var result = Service.GetSongs(id, pageIndex * entriesPerPage, entriesPerPage);
-			var data = new PagingData<SongTagUsageContract>(result.Items.ToPagedList(pageIndex, entriesPerPage, result.TotalCount), id, "SongTagUsages", "ui-tabs-3");
-
-			if (Request.IsAjaxRequest())
-				return PartialView("SongTagUsages", data);
-			else
-				return View("SongTagUsages", data);
 
 		}
 
