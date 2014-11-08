b128fcaac862bd7355519c6730da07a938700fa6
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-11-08 22:38:55 +0000

    shu-t

--- VocaDb/VocaDbWeb/Code/SloganGenerator.cs
+++ VocaDb/VocaDbWeb/Code/SloganGenerator.cs
@@ -5,8 +5,11 @@ namespace VocaDb.Web.Code {
 	public class SloganGenerator {
 
 		private static readonly string[] slogans = {
-			"Telling you who's whoo.", "1st place to check.", 
-			"We got APIs and ApiMikus.", "Use the search, Luka!"
+			"Telling you who's whoo.", 
+			"1st place to check.", 
+			"We got APIs and ApiMikus.", 
+			"Use the search, Luka!",
+			"shu-t up and enjoy"
 		};
 
 		public static string Generate() {
