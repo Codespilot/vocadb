0bf2b690b762ade12168e610bb27574bcb3df2d6
riipah@gmail.com <riipah@gmail.com@1e34815e-c4fa-d4c5-29fa-6120c6c76bdf>
2014-11-15 18:32:37 +0000

    working on client-side PV edit

---
+++ VocaDb/VocaDbWeb/Scripts/ViewModels/PVs/PVEditViewModel.ts
@@ -0,0 +1,43 @@
+﻿
+module vdb.viewModels.pvs {
+
+	import dc = vdb.dataContracts;
+
+	export class PVEditViewModel {
+		
+		constructor(contract: dc.pvs.PVContract, pvType?: string) {
+
+			this.author = contract.author;
+			this.id = contract.id;
+			this.length = contract.length;
+			this.pvId = contract.pvId;
+			this.service = contract.service;
+			this.pvType = pvType || contract.pvType;
+			this.thumbUrl = contract.thumbUrl;
+			this.url = contract.url;
+
+			this.name = ko.observable(contract.name);
+
+		}
+
+		author: string;
+
+		id: number;
+
+		length: number;
+
+		name: KnockoutObservable<string>;
+
+		pvId: string;
+
+		service: string;
+
+		pvType: string;
+
+		thumbUrl: string;
+
+		url: string;
+
+	}
+
+}
