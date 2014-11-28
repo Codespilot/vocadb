4d9bfda2a512b0b3e505e71212bf7b55311cb42f
riipah <riipah@gmail.com>
2014-11-28 20:09:33 +0000

    cleaning up ArtistForEditContract

--- VocaDb/VocaDbModel/DataContracts/UseCases/ArtistForEditContract.cs
+++ VocaDb/VocaDbModel/DataContracts/UseCases/ArtistForEditContract.cs
@@ -9,8 +9,6 @@ namespace VocaDb.Model.DataContracts.UseCases {
 	[DataContract(Namespace = Schemas.VocaDb)]
 	public class ArtistForEditContract : ArtistContract {
 
-		public const int MaxAlbums = 200;
-
 		public ArtistForEditContract() { }
 
 		public ArtistForEditContract(Artist artist, ContentLanguagePreference languagePreference)
@@ -18,10 +16,9 @@ namespace VocaDb.Model.DataContracts.UseCases {
 
 			AllNames = string.Join(", ", artist.AllNames.Where(n => n != Name));
 			BaseVoicebank = artist.BaseVoicebank != null ? new ArtistContract(artist.BaseVoicebank, languagePreference) : null;
+			DefaultNameLanguage = artist.TranslatedName.DefaultLanguage;
 			Description = artist.Description;
 			Groups = artist.Groups.Select(g => new GroupForArtistContract(g, languagePreference)).OrderBy(g => g.Group.Name).ToArray();
-			TranslatedName = new TranslatedStringContract(artist.TranslatedName);
-			Members = artist.Members.Select(m => new GroupForArtistContract(m, languagePreference)).OrderBy(a => a.Member.Name).ToArray();
 			Names = artist.Names.Select(n => new LocalizedStringWithIdContract(n)).ToArray();
 			Pictures = artist.Pictures.Select(p => new EntryPictureFileContract(p)).ToArray();
 			UpdateNotes = string.Empty;
@@ -36,16 +33,13 @@ namespace VocaDb.Model.DataContracts.UseCases {
 		public ArtistContract BaseVoicebank { get; set; }
 
 		[DataMember]
+		public ContentLanguageSelection DefaultNameLanguage { get; set; }
+
+		[DataMember]
 		public string Description { get; set; }
 
 		[DataMember]
 		public GroupForArtistContract[] Groups { get; set; }
-
-		[DataMember]
-		public TranslatedStringContract TranslatedName { get; set; }
-
-		[DataMember]
-		public GroupForArtistContract[] Members { get; set; }
 
 		[DataMember]
 		public LocalizedStringWithIdContract[] Names { get; set; }
@@ -53,6 +47,7 @@ namespace VocaDb.Model.DataContracts.UseCases {
 		[DataMember]
 		public EntryPictureFileContract[] Pictures { get; set; }
 
+		[DataMember]
 		public string UpdateNotes { get; set; }
 
 		[DataMember]
